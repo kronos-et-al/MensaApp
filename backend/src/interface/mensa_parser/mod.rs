7d2d6ba60c788d3dfb3f0de59cdf27bf9427333b
Whatsuup <alexanderalbers01@gmail.com>
2023-06-28 16:18:47 +0200

    created MealplanParser trait

--- backend/src/interface/mensa_parser/mod.rs
+++ backend/src/interface/mensa_parser/mod.rs
@@ -1,1 +1,14 @@
 //! This interface allows the reading of menus from an external source.
+use chrono::{DateTime, Local};
+use crate::interface::mensa_parser::model::ParseCanteen;
+use async_trait::async_trait;
+
+mod model;
+
+#[async_trait] /// Parser interface. Provides functions which return canteen structs. Canteen structs contain raw data obtained by parsing mealplans.
+pub trait MealplanParser {
+    /// Initiate a parse procedure. Returns a canteen struct containing mealplan data of the given date.
+    async fn parse(day: DateTime<Local>) -> Vec<ParseCanteen>;
+    /// Initiate a parse procedure. Returns a tuple containing mealplan data of the next four weeks. The tuple contains a canteen struct with the related date.
+    async fn parse_all() -> (DateTime<Local>, Vec<ParseCanteen>);
+}
