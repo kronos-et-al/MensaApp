8bba2fc554772b32f48482638c7e39d477031da6
Whatsuup <alexanderalbers01@gmail.com>
2023-06-28 21:50:10 +0200

    created result alias for image hoster

--- backend/src/interface/image_hoster/mod.rs
+++ backend/src/interface/image_hoster/mod.rs
@@ -2,15 +2,18 @@
 
 mod model;
 
-use async_trait::async_trait;
 use crate::interface::image_hoster::model::{ImageHosterError, ImageMetaData};
+use async_trait::async_trait;
 
-#[async_trait] /// This trait provides essential functions, which are necessary for image validation.
+pub type Result<T> = std::result::Result<T, ImageHosterError>;
+
+#[async_trait]
+/// This trait provides essential functions, which are necessary for image validation.
 pub trait ImageHoster {
     /// Checks if the given link is valid and provides additional information (ImageMetaData) from the hoster.
-    async fn validate_url(url: String) -> Result<ImageMetaData, ImageHosterError>;
+    async fn validate_url(url: String) -> Result<ImageMetaData>;
     /// Checks if an image still exists at the hoster website.
-    async fn check_existence(photo_id: String) -> Result<bool, ImageHosterError>;
+    async fn check_existence(photo_id: String) -> Result<bool>;
     /// Checks whether the licence is acceptable for our purposes.
-    async fn check_licence(photo_id: String) -> Result<bool, ImageHosterError>;
+    async fn check_licence(photo_id: String) -> Result<bool>;
 }
