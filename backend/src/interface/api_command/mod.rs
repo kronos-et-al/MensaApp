9c7c49b05d78746294d8209ca60a885115f6c68c
Jonatan Ziegler <udslu@student.kit.edu>
2023-06-28 19:28:04 +0200

    added command interface

--- backend/src/interface/api_command/mod.rs
+++ backend/src/interface/api_command/mod.rs
@@ -1,1 +1,56 @@
 //! This interface allows to execute API commands.
+
+use std::error::Error;
+
+use async_trait::async_trait;
+use thiserror::Error;
+use uuid::Uuid;
+
+use crate::util::ReportReason;
+
+pub type Result<T> = std::result::Result<T, CommandError>;
+
+/// Interface for accessing commands which can be triggered by an API.
+#[async_trait]
+pub trait Command {
+    /// Command to report an image. It als gets checked whether the image shall get hidden.
+    async fn report_image(image_id: Uuid, reason: ReportReason, auth_info: AuthInfo) -> Result<()>;
+
+    /// Command to vote up an image. All down-votes of the same user get removed.
+    async fn add_image_upvote(image_id: Uuid, auth_info: AuthInfo) -> Result<()>;
+
+    /// Command to vote down an image. All up-votes of the same user get removed.
+    async fn add_image_downvote(image_id: Uuid, auth_info: AuthInfo) -> Result<()>;
+
+    /// Command to remove an up-vote for an image.
+    async fn remove_image_upvote(image_id: Uuid, auth_info: AuthInfo) -> Result<()>;
+
+    /// Command to remove an down-vote for an image.
+    async fn remove_image_downvote(image_id: Uuid, auth_info: AuthInfo) -> Result<()>;
+
+    /// Command to link an image to a meal.
+    async fn add_image(meal_id: Uuid, image_url: String, auth_info: AuthInfo) -> Result<()>;
+
+    /// command to add a rating to a meal.
+    async fn set_meal_rating(meal_id: Uuid, rating: u32, auth_info: AuthInfo) -> Result<()>;
+}
+
+/// Structure containing all information necessary for authenticating a client.
+pub struct AuthInfo {
+    /// Id of client, chosen at random.
+    pub client_id: Uuid,
+    /// First 10 letters of api key.
+    pub api_ident: String,
+    /// SHA-512 hash of all request parameters, the client id and the name of the request.
+    /// This hash has to be checked to authenticate a command.
+    pub hash: String,
+}
+
+/// Enum describing the possible ways, a command can fail.
+#[derive(Debug, Error)]
+pub enum CommandError {
+    #[error("invalid authentication information provided")]
+    BadAuth,
+    #[error("internal error ocurred")]
+    InternalError(#[from] Box<dyn Error>),
+}
