ba85bb81ab043b6b4e1583ed7e8d6849b613c2c7
Whatsuup <alexanderalbers01@gmail.com>
2023-06-28 11:57:23 +0200

    fixed mealplan naming

--- backend/src/interface/mod.rs
+++ backend/src/interface/mod.rs
@@ -4,6 +4,6 @@ pub mod admin_notification;
 pub mod api_command;
 pub mod image_hoster;
 pub mod image_review;
-pub mod melplan_management;
+pub mod mealplan_management;
 pub mod mensa_parser;
 pub mod persistent_data;
