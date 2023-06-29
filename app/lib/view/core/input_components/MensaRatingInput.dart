a680c5e40728fb1f4f8177a3432a51f32fe9c754
Alexander Kutschera <mail@alexkutschera.de>
2023-06-29 10:56:45 +0200

    commented MensaRatingInput

--- app/lib/view/core/input_components/MensaRatingInput.dart
+++ app/lib/view/core/input_components/MensaRatingInput.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// A input component that is used in the Mensa app and enables the user to enter a 1-5 star rating.
 class MensaRatingInput extends StatelessWidget {
   final Function(int) _onChanged;
   final int _value;
@@ -7,6 +8,14 @@ class MensaRatingInput extends StatelessWidget {
   final Color? _color;
   final bool _disabled;
 
+  /// Creates a new MensaRatingInput.
+  /// @param key The key to identify this widget.
+  /// @param onChanged The function that is called when the rating is changed.
+  /// @param value The current rating.
+  /// @param max The maximum rating.
+  /// @param color The color of the stars.
+  /// @param disabled Whether the input is disabled.
+  /// @returns A new MensaRatingInput.
   const MensaRatingInput(
       {super.key,
       required Function(int) onChanged,
@@ -20,6 +29,9 @@ class MensaRatingInput extends StatelessWidget {
         _color = color,
         _disabled = disabled;
 
+  /// Builds the widget.
+  /// @param context The context in which the widget is built.
+  /// @returns The widget.
   @override
   Widget build(BuildContext context) {
     return Row(
