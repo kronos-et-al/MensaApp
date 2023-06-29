eb7f5a6890bf7bfe860a4a70e0f84bfb54a4bc15
Whatsuup <alexanderalbers01@gmail.com>
2023-06-29 20:24:11 +0200

    async mensa parse scheduling 2

--- backend/src/interface/mealplan_management/mod.rs
+++ backend/src/interface/mealplan_management/mod.rs
@@ -1,5 +1,7 @@
 //! This interface allows starting the operations for updating the menu from the the canteen's website.
+use async_trait::async_trait;
 
+#[async_trait]
 pub trait MensaParseScheduling {
     /// Initiate the parsing procedure of the canteen-website.
     /// Only parse meals of the current date.
