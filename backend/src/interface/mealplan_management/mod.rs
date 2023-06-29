1b5d694ac7e42a782072817844cb06406e6d837f
Whatsuup <alexanderalbers01@gmail.com>
2023-06-29 20:23:07 +0200

    async mensa parse scheduling

--- backend/src/interface/mealplan_management/mod.rs
+++ backend/src/interface/mealplan_management/mod.rs
@@ -3,9 +3,9 @@
 pub trait MensaParseScheduling {
     /// Initiate the parsing procedure of the canteen-website.
     /// Only parse meals of the current date.
-    fn start_update_parsing();
+    async fn start_update_parsing();
 
     /// Initiate the parsing procedure of the canteen-website.
     /// Only parse meals for the next four weeks.
-    fn start_full_parsing();
+    async fn start_full_parsing();
 }
