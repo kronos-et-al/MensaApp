27002bd3ce7540c51f9879aff9244f1ecbd2d7ea
Alexander Kutschera <mail@alexkutschera.de>
2023-06-29 11:02:52 +0200

    commented MensaSlider

--- app/lib/view/core/selection_components/MensaSlider.dart
+++ app/lib/view/core/selection_components/MensaSlider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// A slider that is used in the Mensa app and enables the user to select a value from a range.
 class MensaSlider extends StatelessWidget {
   final void Function(double)? _onChanged;
   final double _value;
@@ -7,6 +8,14 @@ class MensaSlider extends StatelessWidget {
   final double _max;
   final int? _divisions;
 
+  /// Creates a new MensaSlider.
+  /// @param key The key to identify this widget.
+  /// @param onChanged The function that is called when the value is changed.
+  /// @param value The current value.
+  /// @param min The minimum value.
+  /// @param max The maximum value.
+  /// @param divisions The number of discrete divisions.
+  /// @returns A new MensaSlider.
   const MensaSlider(
       {super.key,
       required onChanged,
@@ -20,6 +29,9 @@ class MensaSlider extends StatelessWidget {
         _max = max,
         _divisions = divisions;
 
+  /// Builds the widget.
+  /// @param context The context in which the widget is built.
+  /// @returns The widget.
   @override
   Widget build(BuildContext context) {
     return SliderTheme(
