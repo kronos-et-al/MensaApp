e9aa8cb59b1e62ee1fa8692c602af90d0e3b6b16
Alexander Kutschera <mail@alexkutschera.de>
2023-06-29 11:03:26 +0200

    commented MensaToggle

--- app/lib/view/core/selection_components/MensaToggle.dart
+++ app/lib/view/core/selection_components/MensaToggle.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
+/// A toggle that is used in the Mensa app.
 class MensaToggle extends StatelessWidget {
 
   final void Function(bool)? _onChanged;
   final bool _value;
   final String _label;
 
+  /// Creates a new MensaToggle.
+  /// @param key The key to identify this widget.
+  /// @param onChanged The function that is called when the value is changed.
+  /// @param value The current value.
+  /// @param label The label of the toggle.
+  /// @returns A new MensaToggle.
   const MensaToggle({super.key, required onChanged, required value, required label}): _onChanged = onChanged, _value = value, _label = label;
 
+  /// Builds the widget.
+  /// @param context The context in which the widget is built.
+  /// @returns The widget.
   @override
   Widget build(BuildContext context) {
     return Padding(padding: EdgeInsets.all(8), child: Row(
