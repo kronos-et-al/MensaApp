87b6a350c7e4dd0633656a14945a2d22c2ef5d92
Elena Häußler <uqhnu@student.kit.edu>
2023-06-29 19:14:46 +0200

    Result, Success and Failure implemented

--- app/lib/view_model/repository/error_handling/Result.dart
+++ app/lib/view_model/repository/error_handling/Result.dart
@@ -1,3 +1,14 @@
 sealed class Result<S> {
   const Result();
 }
+
+final class Success<S> extends Result<S> {
+  const Success(this.value);
+  final S value;
+}
+
+final class Failure<S> extends Result<S> {
+  const Failure(this.value);
+  final Exception value;
+}
+
