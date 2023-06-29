2d420f72024074ddf5ef7a7731a5ee1a01042904
Alexander Kutschera <mail@alexkutschera.de>
2023-06-29 11:01:29 +0200

    commented MensaDropdown

--- app/lib/view/core/selection_components/MensaDropdown.dart
+++ app/lib/view/core/selection_components/MensaDropdown.dart
@@ -1,11 +1,18 @@
 import 'package:app/view/core/selection_components/MensaDropdownEntry.dart';
 import 'package:flutter/material.dart';
 
+/// A dropdown that is used in the Mensa app.
 class MensaDropdown<T> extends StatelessWidget {
   final void Function(T?)? _onChanged;
   final T _value;
   final List<MensaDropdownEntry<T>> _items;
 
+  /// Creates a new MensaDropdown.
+  /// @param key The key to identify this widget.
+  /// @param onChanged The function that is called when the value changes.
+  /// @param value The value that is currently selected.
+  /// @param items The items that can be selected.
+  /// @returns A new MensaDropdown.
   const MensaDropdown(
       {super.key,
       required Function(T?)? onChanged,
@@ -15,16 +22,19 @@ class MensaDropdown<T> extends StatelessWidget {
         _value = value,
         _items = items;
 
+  /// Builds the widget.
+  /// @param context The context in which the widget is built.
+  /// @returns The widget.
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Container( // Container is used to give the dropdown a background color.
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(4.0),
           color: Theme.of(context).colorScheme.surface,
         ),
         child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: DropdownButtonHideUnderline(
+            child: DropdownButtonHideUnderline( // DropdownButtonHideUnderline is used to hide the underline of the dropdown.
                 child: DropdownButton<T>(
               elevation: 0,
               borderRadius: BorderRadius.circular(4.0),
