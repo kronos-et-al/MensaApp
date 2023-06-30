e0d54a92475c3138603f0f1db1466f75424ca7f1
Elena Häußler <uqhnu@student.kit.edu>
2023-06-30 11:18:42 +0200

    documentation

--- app/lib/view_model/repository/interface/IServerAccess.dart
+++ app/lib/view_model/repository/interface/IServerAccess.dart
@@ -8,39 +8,55 @@ import '../error_handling/Result.dart';
 
 abstract class IServerAccess {
   /// This method requests all mealplans of all canteens for the next seven days from the server.
+  /// @return The result of the update or an error
   Future<Result<List<Mealplan>>> updateAll();
 
   /// This method requests the mealplan for the committed date of the committed canteen from the server.
+  /// @param date The date of the mealplan
+  /// @param canteen The canteen of the mealplan
+  /// @return The mealplan of the committed date of the committed canteen or an error
   Future<Result<List<Mealplan>>> updateCanteen(Canteen canteen, DateTime date);
 
   /// This method returns the meal with the committed id.
+  /// @param id The id of the meal
+  /// @return The meal with the committed id or an error
   Future<Result<Meal>> getMealFromId(String id);
 
   /// This method updates the rating of the committed meal on the server.
-  /// It returns false, if the rating could not be changed.
+  /// @param rating The new rating of the meal
+  /// @param meal The meal that should be updated
+  /// @return The result of the update. It returns false, if the rating could not be changed.
   Future<bool> updateMealRating(int rating, Meal meal);
 
   /// This method link the committed url to the committed image.
-  /// It returns false, if the image could not be connected to the meal. For example because of an invalid link.
+  /// @param url The url of the image
+  /// @param meal The meal displayed on the image
+  /// @return The result of the update. It returns false, if the image could not be connected to the meal. For example because of an invalid link.
   Future<bool> linkImage(String url, Meal meal);
 
   /// This method adds an upvote to an image.
-  /// It returns false, if the upvote could not be added.
+  /// @param image The image that should be upvoted.
+  /// @return The result of the update. It returns false, if the upvote could not be added.
   Future<bool> upvoteImage(Image image);
 
   /// This method adds an downvote to an image.
-  /// It returns false, if the downvote could not be added.
+  /// @param image The image that should be downvoted.
+  /// @return The result of the update. It returns false, if the downvote could not be added.
   Future<bool> downvoteImage(Image image);
 
   /// This method removes an upvote to an image.
-  /// It returns false, if the upvote could not be removed.
+  /// @param image The image whose upvote should be deleted.
+  /// @return The result of the update. It returns false, if the upvote could not be removed.
   Future<bool> deleteUpvote(Image image);
 
   /// This method removes an downvote to an image.
-  /// It returns false, if the downvote could not be removed.
+  /// @param image The image whose downvote should be deleted.
+  /// @return The result of the update. It returns false, if the downvote could not be removed.
   Future<bool> deleteDownvote(Image image);
 
   /// This method reports an image and sends the committed report reason to the server.
-  /// It returns false, if the report failed.
+  /// @param image The image that should be reported.
+  /// @param reportReason The reason why the image should be reported.
+  /// @return The result of the update. It returns false, if the report failed.
   Future<bool> reportImage(Image image, ReportCategory reportReason);
 }
