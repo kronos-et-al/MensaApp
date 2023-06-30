512e747a9839ead4d1aa0a9d5d7ec6febb5ef551
Elena Häußler <uqhnu@student.kit.edu>
2023-06-30 11:28:54 +0200

    documentation

--- app/lib/view_model/repository/error_handling/NoConnectionException.dart
+++ app/lib/view_model/repository/error_handling/NoConnectionException.dart
@@ -1,6 +1,9 @@
 /// This exception is thrown if the requested data are not stored in the local database and the connection to the server can not be established.
 class NoConnectionException implements Exception {
+  /// The message of the exception.
   String message;
 
+  /// This constructor creates a new NoConnectionException with the given message.
+  /// @param message The message of the exception.
   NoConnectionException(this.message);
 }
--- app/lib/view_model/repository/error_handling/NoMealException.dart
+++ app/lib/view_model/repository/error_handling/NoMealException.dart
@@ -1,6 +1,9 @@
 /// This exception is thrown if the requested meal is not stored in the local database.
 class NoMealException implements Exception {
+  /// The message of the exception.
   String message;
 
+  /// This constructor creates a new NoMealException with the given message.
+  /// @param message The message of the exception.
   NoMealException(this.message);
 }
