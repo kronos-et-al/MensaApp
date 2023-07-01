468e60c42a039bfc46cbe62037e68c0502b3320a
Alexander Kutschera <mail@alexkutschera.de>
2023-07-01 11:54:08 +0200

    implemented MealMainEntry and MealSideEntry

---
+++ app/lib/view/core/information_display/MealSideEntry.dart
@@ -0,0 +1,37 @@
+import 'package:app/view/core/icons/MealIcon.dart';
+import 'package:app/view_model/repository/data_classes/meal/Side.dart';
+import 'package:app/view_model/repository/data_classes/settings/PriceCategory.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class MealSideEntry extends StatelessWidget {
+  final Side _side;
+  final NumberFormat _priceFormat =
+  NumberFormat.currency(locale: 'de_DE', symbol: '€');
+
+  MealSideEntry({Key? key, required Side side})
+      : _side = side,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          MealIcon(foodType: _side.foodType, width: 24, height: 24),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Text('+ ${_side.name}',
+                style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 14, height: 1.5)),
+          ),
+          const SizedBox(width: 8),
+          // TODO use correct price
+          Text(_priceFormat
+              .format(_side.price.getPrice(PriceCategory.student) / 100), style: const TextStyle(fontSize: 14, height: 1.5, )),
+        ],
+      ),
+    );
+  }
+}
