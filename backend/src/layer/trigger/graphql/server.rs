70e0b5851adab29f0b4eefbc77d91493f62e2291
Jonatan Ziegler <udslu@student.kit.edu>
2023-07-02 11:30:02 +0200

    webserver start and stop

--- backend/src/layer/trigger/graphql/server.rs
+++ backend/src/layer/trigger/graphql/server.rs
@@ -1,9 +1,9 @@
 //! This package contains the server that is responsible for providing the graphql API.
 
-use std::fmt;
+use std::{borrow::BorrowMut, future::Future, pin::Pin, sync::Arc, net::Shutdown};
 
 use async_graphql::{
-    http::{playground_source, GraphQLPlaygroundConfig, GraphiQLSource},
+    http::{playground_source, GraphQLPlaygroundConfig},
     EmptySubscription, Schema,
 };
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
@@ -12,6 +12,7 @@ use axum::{
     routing::get,
     Extension, Router, Server,
 };
+use tokio::sync::Notify;
 
 use crate::interface::{api_command::Command, persistent_data::RequestDataAccess};
 
@@ -21,6 +22,7 @@ type GraphQLSchema = Schema<QueryRoot, MutationRoot, EmptySubscription>;
 /// Class witch controls the webserver for GraphQL requests.
 pub struct GraphQLServer {
     schema: GraphQLSchema,
+    shutdown: Option<Pin<Box<dyn Future<Output = ()>>>>,
 }
 
 impl GraphQLServer {
@@ -34,33 +36,55 @@ impl GraphQLServer {
             .data(command)
             .finish();
 
-        Self { schema }
+        Self {
+            schema,
+            shutdown: None,
+        }
     }
 
     /// Starts the GraphQL-Server. It will be running in the background until [`Self::shutdown()`] is called.
-    pub async fn start(&self) {
-        let listen = "0.0.0.0:8080";
+    pub fn start(&mut self) {
+        let listen = "0.0.0.0:8080"; // TODO Ipv6?
 
         let app = Router::new()
-            .route("/", get(graphiql).post(graphql_handler))
+            .route("/", get(graphql_playground).post(graphql_handler))
             .layer(Extension(self.schema.clone()));
 
-        Server::bind(
+        let server = Server::bind(
             &listen
                 .parse()
-                .expect("could not parse listening ip and port"),
+                .expect("could not parse listening ip and port"), // TODO proper error handling
         )
-        .serve(app.into_make_service())
-        .await
-        .unwrap_or_else(|_| panic!("could not listen on {listen}"));
+        .serve(app.into_make_service());
+
+        let shutdown_notify = Arc::new(Notify::new());
+        let shutdown_notify_sender = shutdown_notify.clone();
+
+        let with_shutdown =
+            server.with_graceful_shutdown(async move { shutdown_notify_sender.notified().await });
+
+        let join_handle = tokio::spawn(with_shutdown);
+
+        let shutdown = async move {
+            shutdown_notify.notify_waiters();
+            println!("notified");
+            join_handle.await.expect("web server should not have panicked").expect("error while waiting for webserver to finish");
+        };
+
+        self.shutdown = Some(Box::pin(shutdown));
     }
 
     /// Stops the GraphQL server.
-    pub fn shutdown(&self) {}
+    pub async fn shutdown(&mut self) {
+        self.shutdown
+            .take()
+            .expect("trying to shutdown server but not running")
+            .await;
+    }
 }
 
 #[allow(clippy::unused_async)]
-async fn graphiql() -> impl IntoResponse {
+async fn graphql_playground() -> impl IntoResponse {
     response::Html(playground_source(GraphQLPlaygroundConfig::new("/")))
 }
 
