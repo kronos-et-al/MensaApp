33813bef29d610dc423eeb96913e3440458ae515
Jonatan Ziegler <udslu@student.kit.edu>
2023-07-02 17:06:35 +0200

    added tracing

--- backend/src/layer/trigger/graphql/bin.rs
+++ backend/src/layer/trigger/graphql/bin.rs
@@ -1,11 +1,23 @@
 use mensa_app_backend::layer::trigger::graphql::*;
-
-
+use tracing::{Level, info};
+use tracing_subscriber::{FmtSubscriber, EnvFilter};
 
 #[tokio::main]
 async fn main() {
+    // setup logging
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        // .with_env_filter(EnvFilter::default())
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+
+    // run server
     let mut server = server::GraphQLServer::new(mock::RequestDatabaseMock, mock::CommandMock);
     server.start();
-    tokio::signal::ctrl_c().await.expect("failed to listen for CTRL-C event");
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to listen for CTRL-C event");
+    info!("stopping server...");
     server.shutdown().await;
 }
