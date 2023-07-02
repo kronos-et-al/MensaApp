a2a54b8efd6ee12f92a4e4c18072cc218391c208
Jonatan Ziegler <udslu@student.kit.edu>
2023-07-02 12:33:19 +0200

    globally allow module name repititions

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,5 +1,6 @@
 #![forbid(unsafe_code)]
 #![warn(clippy::pedantic, clippy::nursery, clippy::unwrap_used)]
+#![allow(clippy::module_name_repetitions)]
 //! # MensaApp-Backend
 //! Backend application for providing and synchronizing meal plan data of the canteens of the Studierendenwerk Karlsruhe. [^1]
 //! This application is designed with a transparent three layer model in mind.
