56ee63d32a6371df2a3ee13838e0e9b4fa46b187
Whatsuup <alexanderalbers01@gmail.com>
2023-07-04 08:21:17 +0200

    added &self to database interfaces

--- backend/src/interface/persistent_data.rs
+++ backend/src/interface/persistent_data.rs
@@ -26,20 +26,21 @@ pub enum DataError {
 /// An interface for checking relations and inserting data structures. The MealplanManagement component uses this interface for database access.
 pub trait MealplanManagementDataAccess {
     /// Determines all canteens with a similar name.
-    async fn get_similar_canteens(similar_name: String) -> Result<Vec<Canteen>>;
+    async fn get_similar_canteens(&self, similar_name: String) -> Result<Vec<Canteen>>;
     /// Determines all lines with a similar name.
-    async fn get_similar_lines(similar_name: String) -> Result<Vec<Line>>;
+    async fn get_similar_lines(&self, similar_name: String) -> Result<Vec<Line>>;
     /// Determines all meals with a similar name.
-    async fn get_similar_meals(similar_name: String) -> Result<Vec<Meal>>;
+    async fn get_similar_meals(&self, similar_name: String) -> Result<Vec<Meal>>;
     /// Determines all sides with a similar name.
-    async fn get_similar_sides(similar_name: String) -> Result<Vec<Side>>;
+    async fn get_similar_sides(&self, similar_name: String) -> Result<Vec<Side>>;
 
     /// Updates an existing canteen entity in the database. Returns the entity.
-    async fn update_canteen(uuid: Uuid, name: String) -> Result<Canteen>;
+    async fn update_canteen(&self, uuid: Uuid, name: String) -> Result<Canteen>;
     /// Updates an existing line entity in the database. Returns the entity.
-    async fn update_line(uuid: Uuid, name: String) -> Result<Line>;
+    async fn update_line(&self, uuid: Uuid, name: String) -> Result<Line>;
     /// Updates an existing meal entity in the database. Returns the entity.
     async fn update_meal(
+        &self,
         uuid: Uuid,
         line_id: Uuid,
         date: Date,
@@ -48,6 +49,7 @@ pub trait MealplanManagementDataAccess {
     ) -> Result<Meal>;
     /// Updates an existing side entity in the database. Returns the entity.
     async fn update_side(
+        &self,
         uuid: Uuid,
         line_id: Uuid,
         date: Date,
@@ -56,11 +58,12 @@ pub trait MealplanManagementDataAccess {
     ) -> Result<Side>;
 
     /// Adds a new canteen entity to the database. Returns the new entity.
-    async fn insert_canteen(name: String) -> Result<Canteen>;
+    async fn insert_canteen(&self, name: String) -> Result<Canteen>;
     /// Adds a new line entity to the database. Returns the new entity.
-    async fn insert_line(name: String) -> Result<Line>;
+    async fn insert_line(&self, name: String) -> Result<Line>;
     /// Adds a new meal entity to the database. Returns the new entity.
     async fn insert_meal(
+        &self,
         name: String,
         meal_type: MealType,
         price: Price,
@@ -70,6 +73,7 @@ pub trait MealplanManagementDataAccess {
     ) -> Result<Meal>;
     /// Adds a new side entity to the database. Returns the new entity.
     async fn insert_side(
+        &self,
         name: String,
         meal_type: MealType,
         price: Price,
@@ -83,45 +87,46 @@ pub trait MealplanManagementDataAccess {
 /// An interface for image related data. The ImageReview component uses this interface for database access.
 pub trait ImageReviewDataAccess {
     /// Returns the first n images sorted by rank which are related to an meal served at the given day.
-    async fn get_n_images_by_rank_date(n: u32, date: Date) -> Result<Vec<Image>>;
+    async fn get_n_images_by_rank_date(&self, n: u32, date: Date) -> Result<Vec<Image>>;
     /// Returns the first n images sorted by rank which are related to an meal served in the next week or which were not validated last week.
-    async fn get_n_images_next_week_by_rank_not_checked_last_week(n: u32) -> Result<Vec<Image>>;
+    async fn get_n_images_next_week_by_rank_not_checked_last_week(&self, n: u32) -> Result<Vec<Image>>;
     /// Returns the first n images sorted by the date of the last check (desc) which were not validated in the last week.
-    async fn get_n_images_by_last_checked_not_checked_last_week(n: u32) -> Result<Vec<Image>>;
+    async fn get_n_images_by_last_checked_not_checked_last_week(&self, n: u32) -> Result<Vec<Image>>;
     /// Removes an image with all relations from the database.
-    async fn delete_image(id: Uuid) -> Result<bool>;
+    async fn delete_image(&self, id: Uuid) -> Result<bool>;
     /// Marks all images identified by the given uuids as checked.
-    async fn mark_as_checked(ids: Vec<Uuid>) -> Result<()>;
+    async fn mark_as_checked(&self, ids: Vec<Uuid>) -> Result<()>;
 }
 
 #[async_trait]
 /// An interface for graphql mutation data manipulation. The Command component uses this interface for database access.
 pub trait CommandDataAccess {
     /// Returns the ImageInfo struct of image.
-    async fn get_image_info(image_id: Uuid) -> Result<ImageInfo>;
+    async fn get_image_info(&self, image_id: Uuid) -> Result<ImageInfo>;
     /// Marks an image as hidden. Hidden images cant be seen by users.
-    async fn hide_image(image_id: Uuid) -> Result<()>;
+    async fn hide_image(&self, image_id: Uuid) -> Result<()>;
     /// Saves an image report
-    async fn add_report(image_id: Uuid, client_id: Uuid, reason: ReportReason) -> Result<()>;
+    async fn add_report(&self, image_id: Uuid, client_id: Uuid, reason: ReportReason) -> Result<()>;
     /// Adds an upvote to the given image. An user can only down- or upvote an image.
-    async fn add_upvote(image_id: Uuid, user_id: Uuid) -> Result<()>;
+    async fn add_upvote(&self, image_id: Uuid, user_id: Uuid) -> Result<()>;
     /// Adds a downvote to the given image. An user can only down- or upvote an image.
-    async fn add_downvote(image_id: Uuid, user_id: Uuid) -> Result<()>;
+    async fn add_downvote(&self, image_id: Uuid, user_id: Uuid) -> Result<()>;
     /// Removes an upvote from the given image.
-    async fn remove_upvote(image_id: Uuid, user_id: Uuid) -> Result<()>;
+    async fn remove_upvote(&self, image_id: Uuid, user_id: Uuid) -> Result<()>;
     /// Removes a downvote from the given image.
-    async fn remove_downvote(image_id: Uuid, user_id: Uuid) -> Result<()>;
+    async fn remove_downvote(&self, image_id: Uuid, user_id: Uuid) -> Result<()>;
     /// Adds an image link to the database. The image will be related to the given meal.
     async fn link_image(
+        &self,
         meal_id: Uuid,
         user_id: Uuid,
         image_hoster_id: String,
         url: String,
     ) -> Result<()>;
     /// Adds a rating to the database. The rating will be related to the given meal and the given user.
-    async fn add_rating(meal_id: Uuid, user_id: Uuid, rating: u32) -> Result<()>;
+    async fn add_rating(&self, meal_id: Uuid, user_id: Uuid, rating: u32) -> Result<()>;
     /// Loads all api_keys from the database.
-    async fn get_api_keys() -> Result<Vec<ApiKey>>;
+    async fn get_api_keys(&self) -> Result<Vec<ApiKey>>;
 }
 
 #[async_trait]
