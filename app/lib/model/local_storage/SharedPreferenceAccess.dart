93ce7da4d7c302dcc5da86558fe90c0df6d35ec4
Elena Häußler <uqhnu@student.kit.edu>
2023-07-05 13:58:38 +0200

    documentation

--- app/lib/model/local_storage/SharedPreferenceAccess.dart
+++ app/lib/model/local_storage/SharedPreferenceAccess.dart
@@ -12,34 +12,38 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../../view_model/repository/data_classes/filter/Sorting.dart';
 import '../../view_model/repository/data_classes/meal/FoodType.dart';
 
+/// This class accesses the shared preferences and uses it as local storage.
 class SharedPreferenceAccess implements ILocalStorage {
-  SharedPreferences pref;
+  final SharedPreferences _pref;
 
-  SharedPreferenceAccess(this.pref);
+  /// This constructor creates a new instance of the shared preference access.
+  /// @param _pref The shared preferences.
+  /// @return A new instance of the shared preference access.
+  SharedPreferenceAccess(this._pref);
 
   @override
   Future<String> getClientIdentifier() async {
-    final clientIdentifier = pref.getString('clientIdentifier') ?? "";
+    final clientIdentifier = _pref.getString('clientIdentifier') ?? "";
     return Future.value(clientIdentifier);
   }
 
   @override
   Future<ColorScheme> getColorScheme() async {
-    final colorScheme = pref.getString('colorScheme');
+    final colorScheme = _pref.getString('colorScheme');
     return Future.value(ColorScheme.values.firstWhere((e) => e.toString() == colorScheme));
   }
 
   @override
   Future<FilterPreferences> getFilterPreferences() async {
     // get data from shared preferences
-    final categories = pref.getStringList('filterCategories');
-    final allergens = pref.getStringList('filterAllergens');
-    final price = pref.getInt('filterPrice');
-    final rating = pref.getInt('filterRating');
-    final frequency = pref.getStringList('filterFrequency');
-    final onlyFavorites = pref.getBool('filterFavorite');
-    final sortedBy = pref.getString('filterSort');
-    final ascending = pref.getBool('filterSortAscending');
+    final categories = _pref.getStringList('filterCategories');
+    final allergens = _pref.getStringList('filterAllergens');
+    final price = _pref.getInt('filterPrice');
+    final rating = _pref.getInt('filterRating');
+    final frequency = _pref.getStringList('filterFrequency');
+    final onlyFavorites = _pref.getBool('filterFavorite');
+    final sortedBy = _pref.getString('filterSort');
+    final ascending = _pref.getBool('filterSortAscending');
 
     // convert values to right format
     List<FoodType>? foodTypeEnum;
@@ -77,57 +81,57 @@ class SharedPreferenceAccess implements ILocalStorage {
 
   @override
   Future<MealPlanFormat> getMealPlanFormat() async {
-    final mealPlanFormat = pref.getString('mealPlanFormat');
+    final mealPlanFormat = _pref.getString('mealPlanFormat');
     return Future.value(MealPlanFormat.values.firstWhere((e) => e.toString() == mealPlanFormat));
   }
 
   @override
   Future<PriceCategory> getPriceCategory() async {
-    final priceCategory = pref.getString('priceCategory');
+    final priceCategory = _pref.getString('priceCategory');
     return Future.value(PriceCategory.values.firstWhere((e) => e.toString() == priceCategory));
 
   }
 
   @override
   Future<void> setClientIdentifier(String identifier) async {
-    await pref.setString('clientIdentifier', identifier);
+    await _pref.setString('clientIdentifier', identifier);
   }
 
   @override
   Future<void> setColorScheme(ColorScheme scheme) async {
-    await pref.setString('colorScheme', scheme.toString());
+    await _pref.setString('colorScheme', scheme.toString());
   }
 
   @override
   Future<void> setFilterPreferences(FilterPreferences filter) async {
-    await pref.setStringList('filterCategories', List.of(filter.categories.map((e) => e.toString())));
-    await pref.setStringList('filterAllergens', List.of(filter.allergens.map((e) => e.toString())));
-    await pref.setInt('filterPrice', filter.price);
-    await pref.setInt('filterRating', filter.rating);
-    await pref.setStringList('filterFrequency', List.of(filter.frequency.map((e) => e.toString())));
-    await pref.setBool('filterFavorite', filter.onlyFavorite);
-    await pref.setString('filterSort', filter.sortedBy.toString());
-    await pref.setBool('filterSortAscending', filter.ascending);
+    await _pref.setStringList('filterCategories', List.of(filter.categories.map((e) => e.toString())));
+    await _pref.setStringList('filterAllergens', List.of(filter.allergens.map((e) => e.toString())));
+    await _pref.setInt('filterPrice', filter.price);
+    await _pref.setInt('filterRating', filter.rating);
+    await _pref.setStringList('filterFrequency', List.of(filter.frequency.map((e) => e.toString())));
+    await _pref.setBool('filterFavorite', filter.onlyFavorite);
+    await _pref.setString('filterSort', filter.sortedBy.toString());
+    await _pref.setBool('filterSortAscending', filter.ascending);
   }
 
   @override
   Future<void> setMealPlanFormat(MealPlanFormat format) async {
-    await pref.setString('mealPlanFormat', format.toString());
+    await _pref.setString('mealPlanFormat', format.toString());
   }
 
   @override
   Future<void> setPriceCategory(PriceCategory category) async {
-    await pref.setString('priceCategory', category.toString());
+    await _pref.setString('priceCategory', category.toString());
   }
 
   @override
   Future<String> getCanteen() async {
-    final canteen = pref.getString('canteen') ?? "";
+    final canteen = _pref.getString('canteen') ?? "";
     return Future.value(canteen);
   }
 
   @override
   Future<void> setCanteen(String canteen) async {
-    await pref.setString('canteen', canteen);
+    await _pref.setString('canteen', canteen);
   }
 }
