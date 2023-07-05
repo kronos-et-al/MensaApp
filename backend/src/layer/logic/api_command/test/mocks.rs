2f5c62c7447916cd5fdf5909068ea35aa2e45e42
uglrl <134957879+uglrl@users.noreply.github.com>
2023-07-05 11:44:12 +0200

    Added implementation for CommandImageHosterMock

--- backend/src/layer/logic/api_command/test/mocks.rs
+++ backend/src/layer/logic/api_command/test/mocks.rs
@@ -3,10 +3,10 @@
 use async_trait::async_trait;
 
 use crate::{
-    interface::persistent_data::{
+    interface::{persistent_data::{
         model::{ApiKey, ImageInfo},
-        CommandDataAccess, Result,
-    },
+        CommandDataAccess, Result as DataResult,
+    }, image_hoster::{ImageHoster, model::ImageMetaData, Result as ImageResult}},
     util::{ReportReason, Uuid, Date},
 };
 
@@ -15,7 +15,7 @@ pub struct CommandDatabaseMock;
 #[async_trait]
 impl CommandDataAccess for CommandDatabaseMock {
     #[doc = " Returns the ImageInfo struct of image."]
-    async fn get_image_info(_image_id: Uuid) -> Result<ImageInfo> {
+    async fn get_image_info(_image_id: Uuid) -> DataResult<ImageInfo> {
         let info = ImageInfo {
             approved: true,
             upload_date: Date::default(),
@@ -29,32 +29,32 @@ impl CommandDataAccess for CommandDatabaseMock {
     }
 
     #[doc = " Marks an image as hidden. Hidden images cant be seen by users."]
-    async fn hide_image(_image_id: Uuid) -> Result<()> {
+    async fn hide_image(_image_id: Uuid) -> DataResult<()> {
         Ok(())
     }
 
     #[doc = " Saves an image report"]
-    async fn add_report(_image_id: Uuid, _client_id: Uuid, _reason: ReportReason) -> Result<()> {
+    async fn add_report(_image_id: Uuid, _client_id: Uuid, _reason: ReportReason) -> DataResult<()> {
         Ok(())
     }
 
     #[doc = " Adds an upvote to the given image. An user can only down- or upvote an image."]
-    async fn add_upvote(_image_id: Uuid, _user_id: Uuid) -> Result<()> {
+    async fn add_upvote(_image_id: Uuid, _user_id: Uuid) -> DataResult<()> {
         Ok(())
     }
 
     #[doc = " Adds a downvote to the given image. An user can only down- or upvote an image."]
-    async fn add_downvote(_image_id: Uuid, _user_id: Uuid) -> Result<()> {
+    async fn add_downvote(_image_id: Uuid, _user_id: Uuid) -> DataResult<()> {
         Ok(())
     }
 
     #[doc = " Removes an upvote from the given image."]
-    async fn remove_upvote(_image_id: Uuid, _user_id: Uuid) -> Result<()> {
+    async fn remove_upvote(_image_id: Uuid, _user_id: Uuid) -> DataResult<()> {
         Ok(())
     }
 
     #[doc = " Removes a downvote from the given image."]
-    async fn remove_downvote(_image_id: Uuid, _user_id: Uuid) -> Result<()> {
+    async fn remove_downvote(_image_id: Uuid, _user_id: Uuid) -> DataResult<()> {
         Ok(())
     }
 
@@ -64,17 +64,17 @@ impl CommandDataAccess for CommandDatabaseMock {
         _meal_id: Uuid,
         _image_hoster_id: String,
         _url: String,
-    ) -> Result<()> {
+    ) -> DataResult<()> {
         Ok(())
     }
 
     #[doc = " Adds a rating to the database. The rating will be related to the given meal and the given user."]
-    async fn add_rating(_meal_id: Uuid, _user_id: Uuid, _rating: u32) -> Result<()> {
+    async fn add_rating(_meal_id: Uuid, _user_id: Uuid, _rating: u32) -> DataResult<()> {
         Ok(())
     }
 
     #[doc = " Loads all api_keys from the database."]
-    async fn get_api_keys() -> Result<Vec<ApiKey>> {
+    async fn get_api_keys() -> DataResult<Vec<ApiKey>> {
         let mut keys = Vec::new();
         for i in 2..10 {
             let key = ApiKey {
@@ -84,5 +84,29 @@ impl CommandDataAccess for CommandDatabaseMock {
             keys.push(key);
         }
         Ok(keys)
+    }
+}
+
+
+pub struct CommandImageHosterMock;
+
+#[async_trait]
+impl ImageHoster for CommandImageHosterMock {
+    /// Checks if the given link is valid and provides additional information (ImageMetaData) from the hoster.
+    async fn validate_url(url: String) -> ImageResult<ImageMetaData> {
+        let meta_data = ImageMetaData {
+            id: String::new(),
+            image_url: url,
+            licence: String::new(),
+        };
+        Ok(meta_data)
+    }
+    /// Checks if an image still exists at the hoster website.
+    async fn check_existence(_photo_id: String) -> ImageResult<bool> {
+        Ok(true)
+    }
+    /// Checks whether the licence is acceptable for our purposes.
+    async fn check_licence(_photo_id: String) -> ImageResult<bool> {
+        Ok(true)
     }
 }
