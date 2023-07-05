a941ac554a2e6b84a15a757924f2d420edabc44b
uglrl <134957879+uglrl@users.noreply.github.com>
2023-07-05 15:12:34 +0200

    Fixed description

--- backend/src/layer/logic/api_command/test/mocks.rs
+++ backend/src/layer/logic/api_command/test/mocks.rs
@@ -1,4 +1,4 @@
-//! This crate contains mocks of [`CommandDataAccess`] for testing.
+//! This crate contains mocks of [`CommandDataAccess`] and [`ImageHoster`] and [`AdminNotification`] for testing.
 
 use async_trait::async_trait;
 
