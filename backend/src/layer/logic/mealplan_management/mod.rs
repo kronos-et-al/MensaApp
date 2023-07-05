f3045ef5884340f2d0b00d5a827b2bd0cfaaaca6
uglrl <134957879+uglrl@users.noreply.github.com>
2023-07-05 16:05:23 +0200

    Added implementation for MealPlanParserMock

--- backend/src/layer/logic/mealplan_management/mod.rs
+++ backend/src/layer/logic/mealplan_management/mod.rs
@@ -1,1 +1,3 @@
 //! This component controls the updating of the menus and transforms the received menu plan data into a format compatible for the database.
+
+pub mod test;
---
+++ backend/src/layer/logic/mealplan_management/test/mod.rs
@@ -0,0 +1,1 @@
+pub mod meal_plan_parser_mock;
