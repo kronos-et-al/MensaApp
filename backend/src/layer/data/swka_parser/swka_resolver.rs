037ce1c4303671e6ddca7bb6c7839dc61bd1a509
Whatsuup <alexanderalbers01@gmail.com>
2023-07-06 19:30:56 +0200

    implemented SwKaResolver, changed functions

--- backend/src/layer/data/swka_parser/swka_resolver.rs
+++ backend/src/layer/data/swka_parser/swka_resolver.rs
@@ -1,4 +1,4 @@
-
+use std::error::Error;
 
 pub struct SwKaResolver;
 
@@ -7,12 +7,31 @@ impl SwKaResolver {
         Self
     }
 
-    //TODO impl get_html
-    pub fn get_html(&self, url: String) -> String {
-        todo!()
+    /// priv
+    //#[tokio::main]
+    async fn get_html(&self, url: String) -> Result<String, Box<dyn Error>> {
+        let resp = reqwest::get(url)
+            .await?
+            .text()
+            .await?;
+        Ok(resp)
     }
-    //TODO impl get_htmls
-    pub fn get_htmls(&self, urls: Vec<String>) -> Vec<String> {
-        todo!()
+
+    /// Calls get_html urls times. Returns multiple html code at once.
+    pub async fn get_htmls(&self, urls: Vec<String>) -> Vec<String> {
+        let mut htmls: Vec<String> = Vec::new();
+
+        for url in urls {
+            let html = self.get_html(url).await;
+            if html.is_ok() {
+                htmls.push(html.unwrap());
+            } else {
+                //tries x times, for each url
+                //Log error
+            }
+
+        }
+
+        htmls
     }
 }
