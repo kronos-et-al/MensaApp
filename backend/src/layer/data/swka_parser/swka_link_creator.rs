99aed3dd8996dd8ee794163ae13f001def822be6
uglrl <134957879+uglrl@users.noreply.github.com>
2023-07-08 16:40:48 +0200

    Implemented functions required for creating links

--- backend/src/layer/data/swka_parser/swka_link_creator.rs
+++ backend/src/layer/data/swka_parser/swka_link_creator.rs
@@ -1,21 +1,67 @@
+use chrono::{Datelike, Days, Local};
+
 use crate::util::Date;
+
+const MENSA_NAMES: [&str; 7] = [
+    "mensa_adenauerring",
+    "mensa_gottesaue",
+    "mensa_moltke",
+    "mensa_x1moltkestrasse",
+    "mensa_erzberger",
+    "mensa_tiefenbronner",
+    "mensa_holzgarten",
+];
+const BASE_URL: &str = "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/";
+const URL_SEPERATOR: char = '/';
+const WEEK_SELECTOR: &str = "?kw=";
+const NUMBER_OF_WEEKS_TO_POLL: u64 = 4;
 
 pub struct SwKaLinkCreator;
 
 impl SwKaLinkCreator {
-
     //See <https://youtrack.friedrich-willhelm-der-schredder.de/articles/PSE-A-115/SwKaLinkCreator> for more information
 
     pub fn new() -> SwKaLinkCreator {
         Self
     }
 
-    //TODO impl get_urls(&self, day: Date) -> Vec<String>
     pub fn get_urls(&self, day: &Date) -> Vec<String> {
-        todo!()
+        let calender_week = Self::get_calender_week(*day);
+        let mut urls = Vec::new();
+        for mensa in MENSA_NAMES {
+            urls.push(format!(
+                "{BASE_URL}{mensa}{URL_SEPERATOR}{WEEK_SELECTOR}{calender_week}"
+            ));
+        }
+        urls
     }
-    //TODO impl get_all_urls(&self) -> (Date, Vec<String>)
+
     pub fn get_all_urls(&self) -> Vec<String> {
-        todo!()
+        let today = Self::get_todays_date();
+        let mut all_urls = Vec::new();
+        for i in 0..NUMBER_OF_WEEKS_TO_POLL {
+            let day = today.checked_add_days(Days::new(i * 7)).expect("HELP!");
+            all_urls.append(&mut self.get_urls(&day));
+        }
+        all_urls
+    }
+
+    fn get_calender_week(day: Date) -> u32 {
+        day.iso_week().week()
+    }
+
+    fn get_todays_date() -> Date {
+        Local::now().date_naive()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{layer::data::swka_parser::swka_link_creator::SwKaLinkCreator};
+
+    #[tokio::test]
+    async fn test_url() {
+        let link_creator = SwKaLinkCreator::new();
+        link_creator.get_all_urls();
     }
 }
