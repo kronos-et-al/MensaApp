866a41f140334845a987cdad5c79726cec23cd55
Elena Häußler <uqhnu@student.kit.edu>
2023-07-09 21:24:11 +0200

    test and corrections PreferenceAccess.dart

--- app/lib/view_model/logic/preference/PreferenceAccess.dart
+++ app/lib/view_model/logic/preference/PreferenceAccess.dart
@@ -7,69 +7,80 @@ import 'package:app/view_model/repository/interface/ILocalStorage.dart';
 import 'package:flutter/foundation.dart';
 
 class PreferenceAccess extends ChangeNotifier implements IPreferenceAccess {
-  late ILocalStorage access;
+  late ILocalStorage _access;
 
   late String _clientIdentifier;
   late ColorScheme _colorScheme;
   late PriceCategory _priceCategory;
   late MealPlanFormat _mealPlanFormat;
 
-  PreferenceAccess(this.access) {
-    _init();
+  // waits until _init() is finished initializing
+  late Future _doneInitialization;
+
+  PreferenceAccess(this._access) {
+    _doneInitialization = _init();
   }
 
   Future<void> _init() async {
-    _clientIdentifier = await access.getClientIdentifier() ?? "";
-    _colorScheme = await access.getColorScheme() ?? ColorScheme.system;
-    _priceCategory = await access.getPriceCategory() ?? PriceCategory.student;
-    _mealPlanFormat = await access.getMealPlanFormat() ?? MealPlanFormat.grid;
+    _clientIdentifier = await _access.getClientIdentifier() ?? "";
+    _colorScheme = await _access.getColorScheme() ?? ColorScheme.system;
+    _priceCategory = await _access.getPriceCategory() ?? PriceCategory.student;
+    _mealPlanFormat = await _access.getMealPlanFormat() ?? MealPlanFormat.grid;
   }
 
   @override
-  Future<String> getClientIdentifier() {
+  Future<String> getClientIdentifier() async {
+    await _doneInitialization;
     return Future.value(_clientIdentifier);
   }
 
   @override
   Future<ColorScheme> getColorScheme() async {
+    await _doneInitialization;
     return Future.value(_colorScheme);
   }
 
   @override
-  Future<MealPlanFormat> getMealPlanFormat() {
+  Future<MealPlanFormat> getMealPlanFormat() async {
+    await _doneInitialization;
     return Future.value(_mealPlanFormat);
   }
 
   @override
-  Future<PriceCategory> getPriceCategory() {
+  Future<PriceCategory> getPriceCategory() async {
+    await _doneInitialization;
     return Future.value(_priceCategory);
   }
 
   @override
   Future<void> setClientIdentifier(String identifier) async {
+    await _doneInitialization;
     _clientIdentifier = identifier;
-    await setClientIdentifier(identifier);
+    await _access.setClientIdentifier(identifier);
     notifyListeners();
   }
 
   @override
   Future<void> setColorScheme(ColorScheme scheme) async {
+    await _doneInitialization;
     _colorScheme = scheme;
-    await setColorScheme(scheme);
+    await _access.setColorScheme(scheme);
     notifyListeners();
   }
 
   @override
   Future<void> setMealPlanFormat(MealPlanFormat format) async {
+    await _doneInitialization;
     _mealPlanFormat = format;
-    await setMealPlanFormat(format);
+    await _access.setMealPlanFormat(format);
     notifyListeners();
   }
 
   @override
   Future<void> setPriceCategory(PriceCategory category) async {
+    await _doneInitialization;
     _priceCategory = category;
-    await setPriceCategory(category);
+    await _access.setPriceCategory(category);
     notifyListeners();
   }
 
