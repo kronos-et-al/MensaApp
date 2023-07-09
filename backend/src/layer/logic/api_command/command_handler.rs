f7d8a5e3bfc7264a8eb2e29b2a2b845c951c3aa4
uglrl <134957879+uglrl@users.noreply.github.com>
2023-07-09 19:20:34 +0200

    Added references to self in functions

--- backend/src/layer/logic/api_command/command_handler.rs
+++ backend/src/layer/logic/api_command/command_handler.rs
@@ -17,9 +17,9 @@ pub struct CommandHandler {
 
 impl CommandHandler {
     pub async fn new(
-        command_data: Box<dyn CommandDataAccess>,
-        admin_notification: Box<dyn AdminNotification>,
-        image_hoster: Box<dyn ImageHoster>,
+        command_data: &dyn CommandDataAccess,
+        admin_notification: &dyn AdminNotification,
+        image_hoster: &dyn ImageHoster,
     ) -> Self {
         let keys: Vec<String> = command_data
             .get_api_keys()
@@ -33,92 +33,46 @@ impl CommandHandler {
         }
     }
 }
+
 #[async_trait]
 impl Command for CommandHandler {
-    #[doc = " Command to report an image. It als gets checked whether the image shall get hidden."]
-    #[must_use]
-    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
-    fn report_image<'async_trait>(
+    /// Command to report an image. It als gets checked whether the image shall get hidden.
+    async fn report_image(
+        &self,
         image_id: Uuid,
         reason: ReportReason,
         auth_info: AuthInfo,
-    ) -> ::core::pin::Pin<
-        Box<dyn ::core::future::Future<Output = Result<()>> + ::core::marker::Send + 'async_trait>,
-    > {
+    ) -> Result<()> {
         todo!()
     }
 
-    #[doc = " Command to vote up an image. All down-votes of the same user get removed."]
-    #[must_use]
-    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
-    fn add_image_upvote<'async_trait>(
-        image_id: Uuid,
-        auth_info: AuthInfo,
-    ) -> ::core::pin::Pin<
-        Box<dyn ::core::future::Future<Output = Result<()>> + ::core::marker::Send + 'async_trait>,
-    > {
+    /// Command to vote up an image. All down-votes of the same user get removed.
+    async fn add_image_upvote(&self, image_id: Uuid, auth_info: AuthInfo) -> Result<()> {
         todo!()
     }
 
-    #[doc = " Command to vote down an image. All up-votes of the same user get removed."]
-    #[must_use]
-    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
-    fn add_image_downvote<'async_trait>(
-        image_id: Uuid,
-        auth_info: AuthInfo,
-    ) -> ::core::pin::Pin<
-        Box<dyn ::core::future::Future<Output = Result<()>> + ::core::marker::Send + 'async_trait>,
-    > {
+    /// Command to vote down an image. All up-votes of the same user get removed.
+    async fn add_image_downvote(&self, image_id: Uuid, auth_info: AuthInfo) -> Result<()> {
         todo!()
     }
 
-    #[doc = " Command to remove an up-vote for an image."]
-    #[must_use]
-    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
-    fn remove_image_upvote<'async_trait>(
-        image_id: Uuid,
-        auth_info: AuthInfo,
-    ) -> ::core::pin::Pin<
-        Box<dyn ::core::future::Future<Output = Result<()>> + ::core::marker::Send + 'async_trait>,
-    > {
+    /// Command to remove an up-vote for an image.
+    async fn remove_image_upvote(&self, image_id: Uuid, auth_info: AuthInfo) -> Result<()> {
         todo!()
     }
 
-    #[doc = " Command to remove an down-vote for an image."]
-    #[must_use]
-    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
-    fn remove_image_downvote<'async_trait>(
-        image_id: Uuid,
-        auth_info: AuthInfo,
-    ) -> ::core::pin::Pin<
-        Box<dyn ::core::future::Future<Output = Result<()>> + ::core::marker::Send + 'async_trait>,
-    > {
+    /// Command to remove an down-vote for an image.
+    async fn remove_image_downvote(&self, image_id: Uuid, auth_info: AuthInfo) -> Result<()> {
         todo!()
     }
 
-    #[doc = " Command to link an image to a meal."]
-    #[must_use]
-    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
-    fn add_image<'async_trait>(
-        meal_id: Uuid,
-        image_url: String,
-        auth_info: AuthInfo,
-    ) -> ::core::pin::Pin<
-        Box<dyn ::core::future::Future<Output = Result<()>> + ::core::marker::Send + 'async_trait>,
-    > {
+    /// Command to link an image to a meal.
+    async fn add_image(&self, meal_id: Uuid, image_url: String, auth_info: AuthInfo) -> Result<()> {
         todo!()
     }
 
-    #[doc = " command to add a rating to a meal."]
-    #[must_use]
-    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
-    fn set_meal_rating<'async_trait>(
-        meal_id: Uuid,
-        rating: u32,
-        auth_info: AuthInfo,
-    ) -> ::core::pin::Pin<
-        Box<dyn ::core::future::Future<Output = Result<()>> + ::core::marker::Send + 'async_trait>,
-    > {
+    /// command to add a rating to a meal.
+    async fn set_meal_rating(&self, meal_id: Uuid, rating: u32, auth_info: AuthInfo) -> Result<()> {
         todo!()
     }
 }
