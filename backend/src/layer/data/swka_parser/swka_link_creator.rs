e6631f8f850c7747d23850a5d893b9df9a62bb81
uglrl <134957879+uglrl@users.noreply.github.com>
2023-07-10 16:44:18 +0200

    Improved tests

--- backend/src/layer/data/swka_parser/swka_link_creator.rs
+++ backend/src/layer/data/swka_parser/swka_link_creator.rs
@@ -71,37 +71,69 @@ mod tests {
 
     use crate::{layer::data::swka_parser::swka_link_creator::SwKaLinkCreator, util::Date};
 
+    const URLS_FOR_NEXT_WEEKS: [&str; 28] = [
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_adenauerring/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_gottesaue/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_moltke/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_x1moltkestrasse/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_erzberger/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_tiefenbronner/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_holzgarten/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_adenauerring/?kw=29",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_gottesaue/?kw=29",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_moltke/?kw=29",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_x1moltkestrasse/?kw=29",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_erzberger/?kw=29",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_tiefenbronner/?kw=29",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_holzgarten/?kw=29",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_adenauerring/?kw=30",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_gottesaue/?kw=30",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_moltke/?kw=30",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_x1moltkestrasse/?kw=30",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_erzberger/?kw=30",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_tiefenbronner/?kw=30",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_holzgarten/?kw=30",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_adenauerring/?kw=31",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_gottesaue/?kw=31",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_moltke/?kw=31",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_x1moltkestrasse/?kw=31",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_erzberger/?kw=31",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_tiefenbronner/?kw=31",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_holzgarten/?kw=31",
+    ];
+
+    const URLS_FOR_CURRENT_WEEK: [&str; 7] = [
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_adenauerring/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_gottesaue/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_moltke/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_x1moltkestrasse/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_erzberger/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_tiefenbronner/?kw=28",
+        "https://www.sw-ka.de/en/hochschulgastronomie/speiseplan/mensa_holzgarten/?kw=28",
+    ];
+
     #[tokio::test]
     async fn test_get_urls() {
         let date = Date::from_ymd_opt(2023, 7, 10);
         assert!(date.is_some());
         let date = date.expect("This case should never occur");
         let result = SwKaLinkCreator::get_urls(date);
-        let expected = read_from_file("src/layer/data/swka_parser/tests/test_urls_single.txt");
-        assert!(expected.is_ok());
-        let expected = expected.expect("This case should never occur");
-        assert_eq!(format!("{result:#?}"), expected);
+        assert_eq!(result, URLS_FOR_CURRENT_WEEK);
     }
 
     #[tokio::test]
     async fn test_get_all_urls() {
+        
         let date = Date::from_ymd_opt(2023, 7, 10);
         assert!(date.is_some());
         let date = date.expect("This case should never occur");
         let result = SwKaLinkCreator::get_all_urls_for_next_weeks_from_date(date);
-        let expected = read_from_file("src/layer/data/swka_parser/tests/test_urls_all.txt");
-        assert!(expected.is_ok());
-        let expected = expected.expect("This case should never occur");
-        assert_eq!(format!("{result:#?}"), expected);
+        assert_eq!(result, URLS_FOR_NEXT_WEEKS);
     }
 
     #[allow(dead_code)]
     fn write_output_to_file(path: &str, data: &[String]) -> std::io::Result<()>{
         let mut output = File::create(path)?;
         write!(output, "{data:#?}")
-    }
-
-    fn read_from_file(path: &str) -> std::io::Result<String> {
-        fs::read_to_string(path)
     }
 }
