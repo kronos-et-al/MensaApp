302ba3d12385e904211afc3e5e2428f72898ac65
Whatsuup <alexanderalbers01@gmail.com>
2023-07-10 21:20:17 +0200

    finished resolve implementation

--- backend/src/layer/logic/mealplan_management/relation_resolver.rs
+++ backend/src/layer/logic/mealplan_management/relation_resolver.rs
@@ -14,6 +14,8 @@ impl<DataAccess> RelationResolver<DataAccess>
 where
     DataAccess: MealplanManagementDataAccess,
 {
+    const fn get_edge_case_meal() -> &'static str {"je 100 g"}
+
     pub const fn new(database: DataAccess) -> Self {
         Self {
             db: database
@@ -21,17 +23,14 @@ where
     }
 
     pub async fn resolve(&self, canteen: ParseCanteen, date: Date) -> Result<(), DataError>{
-        let similar_canteens_result = self.db.get_similar_canteen(&canteen.name).await?;
 
-        let db_canteen = match similar_canteens_result {
+        match self.db.get_similar_canteen(&canteen.name).await? {
             Some(similar_canteen) => self.db.update_canteen(similar_canteen.id, &canteen.name).await?,
             None => self.db.insert_canteen(&canteen.name).await?
         };
 
         for line in canteen.lines {
-            let similar_line_result = self.db.get_similar_line(&line.name).await?;
-
-            let db_line = match similar_line_result {
+            let db_line = match self.db.get_similar_line(&line.name).await? {
                 Some(similar_line) => self.db.update_line(similar_line.id, &line.name).await?,
                 None => self.db.insert_line(&line.name).await?
              };
@@ -43,14 +42,18 @@ where
                 // Or just a meal could be found.
                 if similar_meal_result.is_some() {
                     //TODO get a better solution for .expect
-                    self.db.update_meal(similar_meal_result.expect("Cant fail?").id, db_line.id, date, &dish.name, &dish.price).await?;
+                    self.db.update_meal(similar_meal_result.expect("Cant fail, as meal.is_some?").id, db_line.id, date, &dish.name, &dish.price).await?;
                     // A similar side could be found
                 } else if similar_side_result.is_some() {
                     //TODO get a better solution for .expect
-                    self.db.update_side(similar_side_result.expect("Cant fail?").id, db_line.id, date, &dish.name, &dish.price).await?;
+                    self.db.update_side(similar_side_result.expect("Cant fail; as side.is_some?").id, db_line.id, date, &dish.name, &dish.price).await?;
                     // No similar meal could be found. Dish needs to be determined
+
+                    //TODO better solution for this case. This should work also
+                } else if dish.price.price_student < 150 && !dish.name.contains(Self::get_edge_case_meal()) {
+                    self.db.insert_side(&dish.name, dish.meal_type, &dish.price, date, &dish.allergens, &dish.additives).await?;
                 } else {
-                    todo!()
+                    self.db.insert_meal(&dish.name, dish.meal_type, &dish.price, date, &dish.allergens, &dish.additives).await?;
                 }
             }
         }
