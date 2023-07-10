806ff428b8be8067ab794ed374bc9378c70526a5
Whatsuup <alexanderalbers01@gmail.com>
2023-07-10 22:44:01 +0200

    implemented MealplanManger

--- backend/src/layer/logic/mealplan_management/meal_plan_manager.rs
+++ backend/src/layer/logic/mealplan_management/meal_plan_manager.rs
@@ -1,8 +1,12 @@
 use crate::interface::mealplan_management::MensaParseScheduling;
 use crate::interface::mensa_parser::MealplanParser;
-use crate::interface::persistent_data::MealplanManagementDataAccess;
+use crate::interface::persistent_data::{DataError, MealplanManagementDataAccess};
 use crate::layer::logic::mealplan_management::relation_resolver::RelationResolver;
 use async_trait::async_trait;
+use chrono::{NaiveDate, Utc};
+use chrono::format::parse;
+use crate::interface::mensa_parser::model::ParseCanteen;
+use crate::layer::logic::mealplan_management::util::{error_canteen_resolved, trace_canteen_resolved};
 
 struct MealPlanManager<Parser, DataAccess>
 where
@@ -32,11 +36,27 @@ where
     DataAccess: MealplanManagementDataAccess,
     Parser: MealplanParser,
 {
-    async fn start_update_parsing() {
-        todo!()
+    async fn start_update_parsing(&self) {
+        let date = Utc::now().date_naive();
+
+        for parse_canteen in self.parser.parse(date).await {
+            match self.resolver.resolve(parse_canteen, date).await {
+                Ok(canteen) => trace_canteen_resolved(&parse_canteen.name),
+                Err(e) => error_canteen_resolved(&parse_canteen.name, e)
+            }
+        }
     }
 
-    async fn start_full_parsing() {
-        todo!()
+    async fn start_full_parsing(&self) {
+        let date = Utc::now().date_naive();
+        let parse_tuples = self.parser.parse_all().await;
+        for (date, parse_canteens) in parse_tuples {
+            for parse_canteen in parse_canteens {
+                match self.resolver.resolve(parse_canteen, date).await {
+                    Ok(canteen) => trace_canteen_resolved(&parse_canteen.name),
+                    Err(e) => error_canteen_resolved(&parse_canteen.name, e)
+                }
+            }
+        }
     }
 }
--- backend/src/layer/logic/mealplan_management/util.rs
+++ backend/src/layer/logic/mealplan_management/util.rs
@@ -1,9 +1,10 @@
 use tracing::trace;
+use crate::interface::persistent_data::DataError;
 
 pub fn trace_canteen_resolved(name: &String) {
-    trace!("resolved canteen {} with no errors", name)
+    trace!("resolved canteen '{}' with no errors", name)
 }
 
-pub fn error_canteen_resolved(error: &String) {
-    panic!("resolved canteen {} with errors:", error)
+pub fn error_canteen_resolved(name: &String, error: DataError) {
+    panic!("resolved canteen '{name}' with errors: {error}", name = name, error = error.to_string())
 }
