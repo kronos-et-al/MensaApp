852ab2d2330c61179b83f9b9a528144bcc15d25b
Jonatan Ziegler <udslu@student.kit.edu>
2023-07-10 15:01:07 +0200

    graphql: ensured notify does not get missing

--- backend/src/layer/trigger/graphql/server.rs
+++ backend/src/layer/trigger/graphql/server.rs
@@ -110,7 +110,7 @@ impl GraphQLServer {
         let join_handle = tokio::spawn(with_shutdown);
 
         let shutdown = async move {
-            shutdown_notify.notify_waiters();
+            shutdown_notify.notify_one();
             join_handle
                 .await
                 .expect("web server should not have panicked")
