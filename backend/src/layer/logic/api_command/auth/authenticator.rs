1d6b57d14ddf938e7f627108bc25bab978853950
Jonatan Ziegler <udslu@student.kit.edu>
2023-07-10 20:11:07 +0200

    first authentication implementation

--- backend/src/layer/logic/api_command/auth/authenticator.rs
+++ backend/src/layer/logic/api_command/auth/authenticator.rs
@@ -1,9 +1,13 @@
+use sha2::{Digest, Sha512};
+
 use crate::{
     interface::api_command::{AuthInfo, CommandError},
     util::Uuid,
 };
 
 use super::image_command_type::ImageCommandType;
+
+const MEAL_RATING_COMMAND_NAME: &str = "setRating"; // TODO 
 
 pub struct Authenticator {
     api_keys: Vec<String>,
@@ -24,13 +28,29 @@ impl Authenticator {
         todo!()
     }
 
+
     pub fn authn_meal_rating_command(
         &self,
         auth_info: &AuthInfo,
         meal_id: Uuid,
         rating: u32,
     ) -> Result<(), CommandError> {
-        todo!()
+        let api_key = self.get_api_key(auth_info).ok_or(CommandError::BadAuth)?; // TODO more details in error
+        let res = Sha512::new()
+            .chain_update(MEAL_RATING_COMMAND_NAME)
+            .chain_update(auth_info.client_id)
+            .chain_update(api_key)
+            .chain_update(meal_id)
+            .chain_update(rating.to_le_bytes())
+            .finalize();
+
+        let provided_hash = hex::decode(&auth_info.hash).map_err(|_| CommandError::BadAuth)?;
+
+        if res[..] == provided_hash {
+            Ok(())
+        } else {
+            Err(CommandError::BadAuth)
+        }
     }
 
     pub fn authn_add_image_command(
