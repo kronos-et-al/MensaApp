a7c7db97a5f99c98e18ba3af7fa26640e6fd578d
Whatsuup <alexanderalbers01@gmail.com>
2023-07-11 15:34:05 +0200

    structural changes

--- backend/src/layer/logic/mealplan_management/meal_plan_manager.rs
+++ backend/src/layer/logic/mealplan_management/meal_plan_manager.rs
@@ -4,7 +4,9 @@ use crate::interface::persistent_data::{MealplanManagementDataAccess};
 use crate::layer::logic::mealplan_management::relation_resolver::RelationResolver;
 use async_trait::async_trait;
 use chrono::{Utc};
+use crate::interface::mensa_parser::model::ParseCanteen;
 use crate::layer::logic::mealplan_management::util::{error_canteen_resolved, trace_canteen_resolved};
+use crate::util::Date;
 
 pub struct MealPlanManager<Parser, DataAccess>
 where
@@ -26,6 +28,16 @@ where
             parser: meal_plan_parser,
         }
     }
+
+    async fn start_resolving(&self, parse_canteens: Vec<ParseCanteen>, date: Date) {
+        for parse_canteen in parse_canteens {
+            let name = parse_canteen.name.clone();
+            match self.resolver.resolve(parse_canteen, date).await {
+                Ok(_canteen) => trace_canteen_resolved(&name),
+                Err(e) => error_canteen_resolved(&name, &e)
+            }
+        }
+    }
 }
 
 #[async_trait]
@@ -40,15 +52,8 @@ where
     /// If during resolving an error occurs, the resolver stops and a log will be displayed.<br>
     /// Each successful resolving process is also logged.
     async fn start_update_parsing(&self) {
-        let date = Utc::now().date_naive();
-
-        for parse_canteen in self.parser.parse(date).await {
-            let name = parse_canteen.name.clone();
-            match self.resolver.resolve(parse_canteen, date).await {
-                Ok(_canteen) => trace_canteen_resolved(&name),
-                Err(e) => error_canteen_resolved(&name, &e)
-            }
-        }
+        let today = Utc::now().date_naive();
+        self.start_resolving(self.parser.parse(today).await, today);
     }
 
     /// Similar to `start_update_parsing` this method starts the parsing procedure for all meal plans **for the next four weeks**.<br>
@@ -58,13 +63,7 @@ where
     async fn start_full_parsing(&self) {
         let parse_tuples = self.parser.parse_all().await;
         for (date, parse_canteens) in parse_tuples {
-            for parse_canteen in parse_canteens {
-                let name = parse_canteen.name.clone();
-                match self.resolver.resolve(parse_canteen, date).await {
-                    Ok(_canteen) => trace_canteen_resolved(&name),
-                    Err(e) => error_canteen_resolved(&name, &e)
-                }
-            }
+            self.start_resolving(parse_canteens, date);
         }
     }
 }
