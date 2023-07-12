aea854bd9909ab6494488151a5450575df0b326a
Whatsuup <alexanderalbers01@gmail.com>
2023-07-12 12:53:31 +0200

    added test to swka_parse_manager.rs, fixed clippy issues

--- backend/src/layer/data/swka_parser/swka_parse_manager.rs
+++ backend/src/layer/data/swka_parser/swka_parse_manager.rs
@@ -15,7 +15,7 @@ impl SwKaParseManager {
     }
 
     /// Sorts all canteens by days and urls in a hashmap.<br>
-    /// [ParseCanteens] are grouped for each [Date].
+    /// [`ParseCanteen`]s are grouped for each [Date].
     async fn parse_and_sort_canteens_by_days(
         &self,
         urls: Vec<String>,
@@ -49,19 +49,45 @@ impl MealplanParser for SwKaParseManager {
 
         Ok(map.remove(&day).unwrap_or_default())
     }
+
     /// This method handles the parsing procedure for each day in the next four weeks.
     /// To obtain the requested canteens, the manager calls [SwKaLinkCreator] to create urls for the meal plans.
     /// The [SwKaResolver] loads the html code of the given website behind the urls.
     /// At least the [HTMLParser] interprets the html code into [ParseCanteen] objects.
     /// These objects will be returned.<br>
     /// **Return**<br>
-    /// All [ParseCanteen]s grouped by their date or an error if something in the chain above fails.
-
+    /// All [ParseCanteen]s grouped by their [Date] or an error if something in the chain above fails.
     async fn parse_all(&self) -> Result<Vec<(Date, Vec<ParseCanteen>)>, ParseError> {
         let map = self
             .parse_and_sort_canteens_by_days(SwKaLinkCreator::get_all_urls())
             .await?;
 
         Ok(map.into_iter().collect())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::layer::data::swka_parser::swka_parse_manager::SwKaParseManager;
+
+    fn get_urls() -> Vec<String> {
+        vec![
+            String::from(
+                "https://www.sw-ka.de/de/hochschulgastronomie/speiseplan/mensa_adenauerring/",
+            ),
+            String::from(
+                "https://www.sw-ka.de/de/hochschulgastronomie/speiseplan/mensa_erzberger/",
+            ),
+        ]
+    }
+
+    #[tokio::test]
+    async fn sort_and_parse_canteens() {
+        let manager = SwKaParseManager::_new();
+        let result = manager.parse_and_sort_canteens_by_days(get_urls()).await;
+        match result {
+            Ok(_) => {}
+            Err(e) => print!("{e}"),
+        }
     }
 }
