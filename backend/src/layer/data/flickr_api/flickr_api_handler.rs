81f207a6db1ca06be541fdb83634d3bb2c3b2c72
Whatsuup <alexanderalbers01@gmail.com>
2023-07-20 19:32:12 +0200

    implemented flickr_api_handler.rs

--- backend/src/layer/data/flickr_api/flickr_api_handler.rs
+++ backend/src/layer/data/flickr_api/flickr_api_handler.rs
@@ -19,26 +19,68 @@ pub struct FlickrApiHandler {
 
 impl FlickrApiHandler {
 
-    pub const fn new(info: HosterInfo) -> Result<Self, ImageHosterError> {
+
+
+    pub fn new(info: HosterInfo) -> Result<Self, ImageHosterError> {
         Ok(Self {
             parser: XMLParser::new(),
             request: ApiRequest::new(info.api_key, info.client_timeout, info.client_user_agent)?,
         })
+    }
+
+    fn determine_photo_id(url: &str) -> Result<String, ImageHosterError> {
+        if url.len() > 30 {
+            let splits = &url.split('/');
+            match splits[5] {
+                None => Err(ImageHosterError::DecodeFailed(format!("Couldn't detect photo id in url {}", &url))),
+                Some(s) => Ok(String::from(s))
+            }
+        } else {
+            let splits = &url.split('/');
+            match splits.last() {
+                None => Err(ImageHosterError::DecodeFailed(format!("Couldn't detect photo id in url {}", &url))),
+                Some(s) => Ok(String::from(s))
+            }
+        }
+    }
+
+    fn check_licence(licence: String) -> bool {
+        for s in Self::get_valid_licences() {
+            if s == licence {
+                true
+            }
+        }
+        false
+    }
+
+    // See https://www.flickr.com/services/api/flickr.photos.licenses.getInfo.html for all possible licences.
+    fn get_valid_licences() -> List<String> {
+        vec![
+            String::from("All Rights Reserved"),
+            String::from("No known copyright restrictions"),
+            String::from("Public Domain Dedication (CC0)"),
+            String::from("Public Domain Mark")
+        ]
     }
 }
 
 #[async_trait]
 impl ImageHoster for FlickrApiHandler {
 
-    async fn validate_url(&self, url: &str) -> crate::interface::image_hoster::Result<ImageMetaData> {
-        todo!()
+    async fn validate_url(&self, url: &str) -> Result<ImageMetaData, ImageHosterError> {
+        let photo_id = FlickrApiHandler::determine_photo_id(url)?;
+        self.parser.parse_to_image(self.request.flickr_photos_get_sizes(photo_id)?)?
     }
 
-    async fn check_existence(&self, photo_id: &str) -> crate::interface::image_hoster::Result<bool> {
-        todo!()
+    async fn check_existence(&self, _photo_id: &str) -> bool {
+        match self.request.flickr_photos_get_sizes(photo_id) {
+            Ok(_) => true,
+            Err(_) => false
+        }
     }
 
-    async fn check_licence(&self, photo_id: &str) -> crate::interface::image_hoster::Result<bool> {
-        todo!()
+    async fn check_licence(&self, _photo_id: &str) -> bool {
+        FlickrApiHandler::check_licence(self.parser.get_licence(self.request.flickr_photos_licenses_get_license_history(photo_id)?)?)
+
     }
 }
