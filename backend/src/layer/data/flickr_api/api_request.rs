33798282cca827686bdccff3dcbdcb369c268502
Whatsuup <alexanderalbers01@gmail.com>
2023-07-20 14:01:05 +0200

    added request constants

--- backend/src/layer/data/flickr_api/api_request.rs
+++ backend/src/layer/data/flickr_api/api_request.rs
@@ -5,6 +5,16 @@ pub struct ApiRequest {
 
 impl ApiRequest {
 
+    const TEST_URL: &'static str = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=ca370d51a054836007519a00ff4ce59e&per_page=10";
+
+    const BASE_URL: &'static str = "https://api.flickr.com/services/rest/?method=";
+    const GET_SIZES: &'static str = "flickr.photos.getSizes";
+    const GET_LICENCE_HISTORY: &'static str = "flickr.photos.licences.getLicenceHistory";
+
+    const TAG_API_KEY: &'static str = "&api_key=";
+    const TAG_PHOTO_ID: &'static str = "&photo_id=";
+    const FORMAT: &'static str = "&format=json&nojsoncallback=1";
+
     pub const fn new(api_key: String) -> Self {
         Self {
             api_key
