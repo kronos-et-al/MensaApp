562d44f74226f80b8d4ad31cbf9dce7eb7c67949
Alexander Kutschera <mail@alexkutschera.de>
2023-07-20 08:57:52 +0200

    added icons

---
+++ app/lib/view/core/icons/exceptions/FilterExceptionIcon.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+/// This widget is used to display the exceptions icon for a filter issue
+class FilterExceptionIcon extends StatelessWidget {
+  final double _size;
+  final Color? _color;
+
+  /// Creates an filter icon.
+  /// @param key The key to use for this widget.
+  /// @param size The size of the icon.
+  /// @param color The color of the icon.
+  /// @returns a widget that displays the icon for a filter issue
+  const FilterExceptionIcon({super.key, double size = 24, Color? color})
+      : _size = size,
+        _color = color;
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      'assets/icons/exceptions/filter.svg',
+      width: _size,
+      height: _size,
+      colorFilter: ColorFilter.mode(
+          _color ?? Theme.of(context).colorScheme.onSurface, BlendMode.srcIn),
+    );
+  }
+}
