bc8c2fbba4c9ebc48fa952330e8994ad0f9e5674
uglrl <134957879+uglrl@users.noreply.github.com>
2023-07-20 19:03:47 +0200

    Added logging and docs

--- backend/src/layer/data/mail/mail_sender.rs
+++ backend/src/layer/data/mail/mail_sender.rs
@@ -3,56 +3,68 @@ use async_trait::async_trait;
 use lettre::{transport::smtp::authentication::Credentials, Message, SmtpTransport, Transport};
 
 use crate::{
-    interface::admin_notification::{AdminNotification, ImageReportInfo},
+    interface::{
+        admin_notification::{AdminNotification, ImageReportInfo},
+        persistent_data::{DataError, Result},
+    },
     startup::config::mail_info::MailInfo,
 };
 
+use tracing::{info, warn};
+
 pub struct MailSender {
     config: MailInfo,
+    mailer: SmtpTransport,
 }
 
 impl MailSender {
-    #[must_use]
-    pub const fn new(config: MailInfo) -> Self {
-        Self { config }
+    /// Creates a new [`MailSender`] with the attributes defined in config. Also creates an SMTP connection to the smtp server defined in config
+    /// 
+    /// # Errors
+    /// Returns an error, if the connection could not be established to the smtp server
+    pub fn new(config: MailInfo) -> Result<Self> {
+        let creds = Credentials::new(config.username.clone(), config.password.clone());
+        if let Ok(transport_builder) = SmtpTransport::relay(&config.smtp_server) {
+            let mailer = transport_builder
+                .port(config.smtp_port)
+                .credentials(creds)
+                .build();
+            Ok(Self { config, mailer })
+        } else {
+            Err(DataError::NoSuchItem)
+        }
     }
 }
 
 #[async_trait]
 impl AdminNotification for MailSender {
     async fn notify_admin_image_report(&self, info: ImageReportInfo) {
-        let email = Message::builder()
-            .from(
-                format!("app <{}>", self.config.username.clone())
-                    .parse()
-                    .expect("HELP!"),
-            )
-            .to(format!("admin <{}>", self.config.admin_email_address)
-                .parse()
-                .expect("HELP!"))
-            .subject("Hello")
-            .body(format!("{info:#?}"))
-            .expect("HELP!");
-
-        let creds = Credentials::new(self.config.username.clone(), self.config.password.clone());
-
-        // Open a remote connection to gmail
-        let mailer = SmtpTransport::relay(&self.config.smtp_server)
-            .expect("HELP!")
-            .port(self.config.smtp_port)
-            .credentials(creds)
-            .build();
-
-        // Send the email
-        match mailer.send(&email) {
-            Ok(_) => println!("Email sent successfully!"),
-            Err(e) => panic!("Could not send email: {e:?}"),
+        match format!("app <{}>", self.config.username.clone()).parse() {
+            Err(error) => warn!("The sender could not be created: {error}"),
+            Ok(sender) => match format!("admin <{}>", self.config.admin_email_address).parse() {
+                Err(error) => warn!("The reciever could not be created: {error}"),
+                Ok(reciever) => {
+                    match Message::builder()
+                        .from(sender)
+                        .to(reciever)
+                        .subject("An image was reported for reviewing")
+                        .body(format!("{info:#?}"))
+                    {
+                        Err(error) => warn!("The email could not be created: {error}"),
+                        Ok(email) => match self.mailer.send(&email) {
+                            Ok(_) => info!("Email sent successfully!"),
+                            Err(e) => warn!("Could not send email: {e:?}"),
+                        },
+                    }
+                }
+            },
         }
     }
 }
 
 #[cfg(test)]
 mod test {
+    #![allow(clippy::unwrap_used)]
     use crate::{
         interface::admin_notification::{AdminNotification, ImageReportInfo},
         layer::data::mail::mail_sender::MailSender,
@@ -70,7 +82,7 @@ mod test {
             admin_email_address: String::from(" "),
         };
 
-        let mail_sender = MailSender::new(mail_info);
+        let mail_sender = MailSender::new(mail_info).unwrap();
         let report_info = ImageReportInfo {
             reason: crate::util::ReportReason::Advert,
             image_got_hidden: true,
