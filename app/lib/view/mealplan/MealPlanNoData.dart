7593162d0620c7603128ba2000d9983f90ba0473
Elena Häußler <uqhnu@student.kit.edu>
2023-07-21 19:24:39 +0200

    documentation, add icon and style

--- app/lib/view/mealplan/MealPlanNoData.dart
+++ app/lib/view/mealplan/MealPlanNoData.dart
@@ -1,15 +1,23 @@
+import 'package:app/view/core/icons/exceptions/NoDataExceptionIcon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 
+/// This widget is used to display the exception for no available data on the server.
 class MealPlanNoData extends StatelessWidget {
+  /// Creates a no data widget
+  /// @return a widget that displays the exeption for no available data on the server
+  const MealPlanNoData({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          // todo add icon
-          Text(FlutterI18n.translate(
-              context, "mealplanException.noDataException")),
-        ]));
+    return Column(crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const NoDataExceptionIcon(size: 48),
+          Text(
+            FlutterI18n.translate(context, "mealplanException.noDataException"),
+            style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.5),
+            textAlign: TextAlign.center,
+          ),
+        ]);
   }
-
 }
