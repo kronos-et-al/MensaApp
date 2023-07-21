812ea3c4495676444b0ca14d8fba238b22279798
Elena Häußler <uqhnu@student.kit.edu>
2023-07-21 19:42:58 +0200

    create class

---
+++ app/lib/view/settings/Settings.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
