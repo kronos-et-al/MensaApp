6b213d25fd129200f16e1165378f5f398ef3ad6d
Alexander Kutschera <mail@alexkutschera.de>
2023-07-21 08:18:45 +0200

    implemented MensaAppBar

--- app/lib/view/core/MensaAppBar.dart
+++ app/lib/view/core/MensaAppBar.dart
@@ -7,50 +7,26 @@ import 'package:provider/provider.dart';
 
 class MensaAppBar extends StatelessWidget implements PreferredSizeWidget {
   final PreferredSizeWidget? _bottom;
+  final Widget _child;
   final double? _appBarHeight;
 
-  MensaAppBar({super.key, PreferredSizeWidget? bottom, double? appBarHeight})
+  MensaAppBar(
+      {super.key,
+      PreferredSizeWidget? bottom,
+      double? appBarHeight,
+      required Widget child})
       : _appBarHeight = appBarHeight,
         _bottom = bottom,
         preferredSize =
-            _PreferredAppBarSize(appBarHeight, bottom?.preferredSize.height);
+            _PreferredAppBarSize(appBarHeight, bottom?.preferredSize.height),
+        _child = child;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
         height: preferredSize.height,
         child: Column(children: [
-          Consumer<IMealAccess>(
-              builder: (context, mealAccess, child) => FutureBuilder(
-                    future: mealAccess.getAvailableCanteens(),
-                    builder: (context, availableCanteens) => FutureBuilder(
-                        future: mealAccess.getCanteen(),
-                        builder: (context, selectedCanteen) {
-                          if (selectedCanteen.hasData) {
-                            return SizedBox(
-                                height: _appBarHeight,
-                                child: MensaDropdown<Canteen>(
-                                    backgroundColor: Theme.of(context)
-                                        .colorScheme
-                                        .background,
-                                    onChanged: (canteen) => {
-                                          if (canteen != null)
-                                            {
-                                              mealAccess.changeCanteen(canteen),
-                                            }
-                                        },
-                                    value: selectedCanteen.requireData,
-                                    items: availableCanteens.requireData
-                                        .map((canteen) =>
-                                            MensaDropdownEntry<Canteen>(
-                                              value: canteen,
-                                              label: canteen.name,
-                                            ))
-                                        .toList()));
-                          }
-                          return const SizedBox.shrink();
-                        }),
-                  )),
+          SizedBox(height: _appBarHeight, child: _child),
           if (_bottom != null) _bottom!
         ]));
   }
