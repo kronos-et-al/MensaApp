052f022553b48948315751a81559ce06eac4be89
Whatsuup <alexanderalbers01@gmail.com>
2023-07-23 15:45:55 +0200

    added docs to json parser

--- backend/src/layer/data/flickr_api/json_parser.rs
+++ backend/src/layer/data/flickr_api/json_parser.rs
@@ -1,16 +1,23 @@
+use serde_json::Value::String;
 use crate::interface::image_hoster::model::ImageMetaData;
 use crate::interface::image_hoster::ImageHosterError;
 use crate::layer::data::flickr_api::json_structs::{JsonRoot, JsonRootError, JsonRootLicense, JsonRootSizes};
 
 pub struct JSONParser;
 
-fn GET_SELECTED_SIZE() -> String { String::from("Medium 800") }
+fn get_selected_size_fallback() -> String { String::from("Medium") }
+fn get_selected_size() -> String { String::from("Large") }
 
 impl JSONParser {
-    pub const fn _new() -> Self {
+    pub const fn new() -> Self {
         Self
     }
 
+    /// Obtains the preferred image information from the [`JsonRootSizes`] struct.
+    /// # Return
+    /// The [`ImageMetaData`] struct containing all necessary information for the image.
+    /// If the preferred size cannot be obtained a fallback to a smaller size 'll be done.
+    /// If even this fallback size is not available the url will be empty. No url 'll be provided.
     pub async fn parse_get_sizes(
         &self,
         root: JsonRootSizes,
@@ -18,9 +25,17 @@ impl JSONParser {
     ) -> Result<ImageMetaData, ImageHosterError> {
         let mut url = String::new();
         for size in root.sizes.size {
-            if size.label == GET_SELECTED_SIZE() {
+            if size.label == get_selected_size() {
                 url = size.source;
                 break;
+            }
+        }
+        if url == String::new() {
+            for size in root.sizes.size {
+                if size.label == get_selected_size_fallback() {
+                    url = size.source;
+                    break;
+                }
             }
         }
         Ok(ImageMetaData {
@@ -29,6 +44,10 @@ impl JSONParser {
         })
     }
 
+    /// Obtains and validates the license by the information from the [`JsonRootLicense`] struct.
+    /// # Return
+    /// A boolean if the image has an valid license or not.
+    /// If the image has no license or no license history, the image isn't restricted by any license and true 'll be returned.
     pub fn check_license(&self, root: JsonRootLicense) -> bool {
         let mut last_date: u64 = 0;
         for entry in root.rsp.license_history.into_iter() {
@@ -50,6 +69,9 @@ impl JSONParser {
         false
     }
 
+    /// Obtains and determines an error by his error code and message provided by the [`JsonRootError`] struct.
+    /// # Return
+    /// An [`ImageHosterError`] that fittest be with the Flickr-Error types.
     pub fn parse_error(&self, err_info: JsonRootError) -> ImageHosterError {
         let err_code = err_info.rsp.err.code;
         let err_msg = err_info.rsp.err.msg;
@@ -72,6 +94,7 @@ impl JSONParser {
             String::from("No known copyright restrictions"),
             String::from("Public Domain Dedication (CC0)"),
             String::from("Public Domain Mark"),
+            String::new()
         ]
     }
 }
