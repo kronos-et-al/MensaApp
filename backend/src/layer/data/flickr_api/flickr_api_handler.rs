109461f9cf5c71ff20e9956fb074769f5ada3208
Whatsuup <alexanderalbers01@gmail.com>
2023-07-24 15:34:24 +0200

    implemented photo_id determination, implemented interface requests

--- backend/src/layer/data/flickr_api/flickr_api_handler.rs
+++ backend/src/layer/data/flickr_api/flickr_api_handler.rs
@@ -2,7 +2,6 @@ use crate::interface::image_hoster::model::ImageMetaData;
 use crate::interface::image_hoster::{ImageHoster, ImageHosterError};
 use crate::layer::data::flickr_api::api_request::ApiRequest;
 use async_trait::async_trait;
-use std::ops::Index;
 
 #[derive(Debug, Clone)]
 pub struct HosterInfo {
@@ -20,29 +19,36 @@ impl FlickrApiHandler {
         }
     }
 
-    fn determine_photo_id(url: &str) -> Result<&str, ImageHosterError> {
-        let splits: Vec<&str> = url.split('/').collect();
-        if url.len() > 30 {
-            Ok(splits.index(5)) // TODO match
-        } else {
-            Ok(splits.last().copied().unwrap()) // TODO match
+    // URL TYPE 1: https://www.flickr.com/photos/gerdavs/52310534489/ <- remove last '/'
+    // URL TYPE 2: https://flic.kr/p/2nGvar4
+    // Both cases: Split with '/' and get last member (= photo_id).
+    fn determine_photo_id(&self, mut url: &str) -> Result<&str, ImageHosterError> {
+        if url.ends_with("/") {
+            // remove last '/'
+            let mut chars = url.chars();
+            chars.next_back();
+            url = chars.as_str();
+        }
+        let splits= url.split('/');
+        match splits.last() {
+            None => Err(ImageHosterError::FormatNotFound(format!("this url format is not supported: '{}'", url))),
+            Some(last) => Ok(last)
         }
     }
-
-
 }
 
 #[async_trait]
 impl ImageHoster for FlickrApiHandler {
-    async fn validate_url(&self, _url: &str) -> Result<ImageMetaData, ImageHosterError> {
-        todo!()
+    async fn validate_url(&self, url: &str) -> Result<ImageMetaData, ImageHosterError> {
+        let photo_id = self.determine_photo_id(url)?;
+        self.request.flickr_photos_get_sizes(photo_id).await
     }
 
-    async fn check_existence(&self, _photo_id: &str) -> Result<bool, ImageHosterError> {
-        todo!()
+    async fn check_existence(&self, photo_id: &str) -> Result<bool, ImageHosterError> {
+        self.check_licence(photo_id)
     }
 
-    async fn check_licence(&self, _photo_id: &str) -> Result<bool, ImageHosterError> {
-        todo!()
+    async fn check_licence(&self, photo_id: &str) -> Result<bool, ImageHosterError> {
+        self.request.flickr_photos_licenses_get_license_history(photo_id).await
     }
 }
