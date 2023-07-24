aa8d3bb4da934ee97e33151666e0a6b1e27685d3
uglrl <134957879+uglrl@users.noreply.github.com>
2023-07-24 11:34:00 +0200

    Compacted mocks

--- backend/src/layer/logic/image_review/test/image_review_database_mock.rs
+++ backend/src/layer/logic/image_review/test/image_review_database_mock.rs
@@ -27,14 +27,17 @@ impl ImageReviewDatabaseMock {
             .lock()
             .expect("failed to lock mutex for `n_images_by_rank_date_calls` counter")
     }
+
     #[must_use]
     pub fn get_n_images_next_week_by_rank_not_checked_last_week_calls(&self) -> u32 {
         *self.n_images_next_week_by_rank_not_checked_last_week_calls.lock().expect("failed to lock mutex for `n_images_next_week_by_rank_not_checked_last_week_calls` counter")
     }
+
     #[must_use]
     pub fn get_n_images_by_last_checked_not_checked_last_week_calls(&self) -> u32 {
         *self.n_images_by_last_checked_not_checked_last_week_calls.lock().expect("failed to lock mutex for `n_images_by_last_checked_not_checked_last_week_calls` counter")
     }
+
     #[must_use]
     pub fn get_delete_image_calls(&self) -> u32 {
         *self
@@ -42,6 +45,7 @@ impl ImageReviewDatabaseMock {
             .lock()
             .expect("failed to lock mutex for `delete_image_calls` counter")
     }
+
     #[must_use]
     pub fn get_mark_as_checked_calls(&self) -> u32 {
         *self
@@ -53,67 +57,33 @@ impl ImageReviewDatabaseMock {
 
 #[async_trait]
 impl ImageReviewDataAccess for ImageReviewDatabaseMock {
-    /// Returns the first n images sorted by rank which are related to an meal served at the given day.
     async fn get_n_images_by_rank_date(&self, n: u32, _date: Date) -> Result<Vec<Image>> {
         *self
             .n_images_by_rank_date_calls
             .lock()
             .expect("failed to lock mutex for `n_images_by_rank_date_calls` counter") += 1;
 
-        let mut images = Vec::new();
-        for _i in 0..n {
-            images.push(Image {
-                id: Uuid::default(),
-                image_hoster_id: String::default(),
-                url: String::default(),
-                rank: 0.0,
-                upvotes: 0,
-                downvotes: 0,
-            });
-        }
-        Ok(images)
+        Ok((0..n).map(|_| Image::default()).collect())
     }
-    /// Returns the first n images sorted by rank which are related to an meal served in the next week or which were not validated last week.
+
     async fn get_n_images_next_week_by_rank_not_checked_last_week(
         &self,
         n: u32,
     ) -> Result<Vec<Image>> {
         *self.n_images_next_week_by_rank_not_checked_last_week_calls.lock().expect("failed to lock mutex for `n_images_next_week_by_rank_not_checked_last_week_calls` counter") += 1;
 
-        let mut images = Vec::new();
-        for _i in 0..n {
-            images.push(Image {
-                id: Uuid::default(),
-                image_hoster_id: String::default(),
-                url: String::default(),
-                rank: 0.0,
-                upvotes: 0,
-                downvotes: 0,
-            });
-        }
-        Ok(images)
+        Ok((0..n).map(|_| Image::default()).collect())
     }
-    /// Returns the first n images sorted by the date of the last check (desc) which were not validated in the last week.
+
     async fn get_n_images_by_last_checked_not_checked_last_week(
         &self,
         n: u32,
     ) -> Result<Vec<Image>> {
         *self.n_images_by_last_checked_not_checked_last_week_calls.lock().expect("failed to lock mutex for `n_images_by_last_checked_not_checked_last_week_calls` counter") += 1;
 
-        let mut images = Vec::new();
-        for _i in 0..n {
-            images.push(Image {
-                id: Uuid::default(),
-                image_hoster_id: String::default(),
-                url: String::default(),
-                rank: 0.0,
-                upvotes: 0,
-                downvotes: 0,
-            });
-        }
-        Ok(images)
+        Ok((0..n).map(|_| Image::default()).collect())
     }
-    /// Removes an image with all relations from the database.
+
     async fn delete_image(&self, id: Uuid) -> Result<bool> {
         *self
             .delete_image_calls
@@ -127,7 +97,7 @@ impl ImageReviewDataAccess for ImageReviewDatabaseMock {
             Ok(true)
         }
     }
-    /// Marks all images identified by the given uuids as checked.
+
     async fn mark_as_checked(&self, _id: Uuid) -> Result<()> {
         *self
             .mark_as_checked_calls
