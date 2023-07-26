549c73f6de1ac4cced2e2554f6f9e35055e9e235
Whatsuup <alexanderalbers01@gmail.com>
2023-07-26 17:05:58 +0200

    fixed line position as 0 isn't valid

--- backend/src/layer/data/database/fixtures/line.sql
+++ backend/src/layer/data/database/fixtures/line.sql
@@ -2,4 +2,4 @@ INSERT INTO line(line_id, canteen_id, name, position) VALUES
         ('61b27158-817c-4716-bd41-2a8901391ea4', '10728cc4-1e07-4e18-a9d9-ca45b9782413', 'line 2', 2), 
         ('3e8c11fa-906a-4c6a-bc71-28756c6b00ae', '10728cc4-1e07-4e18-a9d9-ca45b9782413', 'line 1', 1), 
         ('a4956171-a5fc-4c6b-a028-3cb2e5d2bedb', '10728cc4-1e07-4e18-a9d9-ca45b9782413', 'special line', 3),
-        ('119c55b7-e539-4849-bad1-984efff2aad6', '8f10c56d-da9b-4f62-b4c1-16feb0f98c67', 'single line', 0);
+        ('119c55b7-e539-4849-bad1-984efff2aad6', '8f10c56d-da9b-4f62-b4c1-16feb0f98c67', 'single line', 1);
