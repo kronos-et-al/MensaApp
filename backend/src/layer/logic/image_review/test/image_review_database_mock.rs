8f06b2b8e385a544debad2899008cea145117077
uglrl <134957879+uglrl@users.noreply.github.com>
2023-07-27 17:33:40 +0200

    Changed function names

--- backend/src/layer/logic/image_review/test/image_review_database_mock.rs
+++ backend/src/layer/logic/image_review/test/image_review_database_mock.rs
@@ -11,30 +11,36 @@ pub const ID_TO_THROW_ERROR_ON_DELETE: Uuid = Uuid::from_u128(7u128);
 
 #[derive(Default)]
 pub struct ImageReviewDatabaseMock {
-    n_images_by_rank_date_calls: Arc<Mutex<u32>>,
-    n_images_next_week_by_rank_not_checked_last_week_calls: Arc<Mutex<u32>>,
-    n_images_by_last_checked_not_checked_last_week_calls: Arc<Mutex<u32>>,
+    images_for_date_calls: Arc<Mutex<u32>>,
+    unvalidated_images_for_next_week_calls: Arc<Mutex<u32>>,
+    old_images_calls: Arc<Mutex<u32>>,
     delete_image_calls: Arc<Mutex<u32>>,
     mark_as_checked_calls: Arc<Mutex<u32>>,
 }
 
 impl ImageReviewDatabaseMock {
     #[must_use]
-    pub fn get_n_images_by_rank_date_calls(&self) -> u32 {
+    pub fn get_images_for_date_calls(&self) -> u32 {
         *self
-            .n_images_by_rank_date_calls
+            .images_for_date_calls
             .lock()
-            .expect("failed to lock mutex for `n_images_by_rank_date_calls` counter")
+            .expect("failed to lock mutex for `images_for_date_calls` counter")
     }
 
     #[must_use]
-    pub fn get_n_images_next_week_by_rank_not_checked_last_week_calls(&self) -> u32 {
-        *self.n_images_next_week_by_rank_not_checked_last_week_calls.lock().expect("failed to lock mutex for `n_images_next_week_by_rank_not_checked_last_week_calls` counter")
+    pub fn get_unvalidated_images_for_next_week_calls(&self) -> u32 {
+        *self
+            .unvalidated_images_for_next_week_calls
+            .lock()
+            .expect("failed to lock mutex for `unvalidated_images_for_next_week_calls` counter")
     }
 
     #[must_use]
-    pub fn get_n_images_by_last_checked_not_checked_last_week_calls(&self) -> u32 {
-        *self.n_images_by_last_checked_not_checked_last_week_calls.lock().expect("failed to lock mutex for `n_images_by_last_checked_not_checked_last_week_calls` counter")
+    pub fn get_old_images_calls(&self) -> u32 {
+        *self
+            .old_images_calls
+            .lock()
+            .expect("failed to lock mutex for `old_images_calls` counter")
     }
 
     #[must_use]
@@ -56,29 +62,30 @@ impl ImageReviewDatabaseMock {
 
 #[async_trait]
 impl ImageReviewDataAccess for ImageReviewDatabaseMock {
-    async fn get_n_images_by_rank_date(&self, n: u32, _date: Date) -> Result<Vec<Image>> {
+    async fn get_images_for_date(&self, n: u32, _date: Date) -> Result<Vec<Image>> {
         *self
-            .n_images_by_rank_date_calls
+            .images_for_date_calls
             .lock()
-            .expect("failed to lock mutex for `n_images_by_rank_date_calls` counter") += 1;
+            .expect("failed to lock mutex for `images_for_date_calls` counter") += 1;
 
         Ok((0..n).map(|_| Image::default()).collect())
     }
 
-    async fn get_n_images_next_week_by_rank_not_checked_last_week(
-        &self,
-        n: u32,
-    ) -> Result<Vec<Image>> {
-        *self.n_images_next_week_by_rank_not_checked_last_week_calls.lock().expect("failed to lock mutex for `n_images_next_week_by_rank_not_checked_last_week_calls` counter") += 1;
+    async fn get_unvalidated_images_for_next_week(&self, n: u32) -> Result<Vec<Image>> {
+        *self
+            .unvalidated_images_for_next_week_calls
+            .lock()
+            .expect("failed to lock mutex for `unvalidated_images_for_next_week_calls` counter") +=
+            1;
 
         Ok((0..n).map(|_| Image::default()).collect())
     }
 
-    async fn get_n_images_by_last_checked_not_checked_last_week(
-        &self,
-        n: u32,
-    ) -> Result<Vec<Image>> {
-        *self.n_images_by_last_checked_not_checked_last_week_calls.lock().expect("failed to lock mutex for `n_images_by_last_checked_not_checked_last_week_calls` counter") += 1;
+    async fn get_old_images(&self, n: u32) -> Result<Vec<Image>> {
+        *self
+            .old_images_calls
+            .lock()
+            .expect("failed to lock mutex for `old_images_calls` counter") += 1;
 
         Ok((0..n).map(|_| Image::default()).collect())
     }
