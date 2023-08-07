5d7290747c8316c7dab153d84046cd760303eb79
Elena Häußler <uqhnu@student.kit.edu>
2023-08-07 17:15:04 +0200

    add tests cases

--- app/test/view-model/ImageAccessTest.dart
+++ app/test/view-model/ImageAccessTest.dart
@@ -1,5 +1,9 @@
 import 'package:app/view_model/logic/image/ImageAccess.dart';
+import 'package:app/view_model/repository/data_classes/meal/FoodType.dart';
 import 'package:app/view_model/repository/data_classes/meal/ImageData.dart';
+import 'package:app/view_model/repository/data_classes/meal/Meal.dart';
+import 'package:app/view_model/repository/data_classes/meal/Price.dart';
+import 'package:app/view_model/repository/data_classes/settings/ReportCategory.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -8,9 +12,16 @@ import '../model/mocks/ApiMock.dart';
 void main() {
   final api = ApiMock();
 
+  final Meal meal = Meal(
+      id: "42",
+      name: "name",
+      foodType: FoodType.vegetarian,
+      price: Price(student: 100, employee: 200, pupil: 300, guest: 400));
+
   ImageAccess images = ImageAccess(api);
 
-  ImageData image = ImageData(id: "42",
+  ImageData image = ImageData(
+      id: "42",
       url: "url",
       imageRank: 42,
       positiveRating: 42,
@@ -69,6 +80,34 @@ void main() {
       when(() => api.deleteDownvote(image)).thenAnswer((_) async => true);
       await images.deleteDownvote(image);
       expect(image.individualRating, 0);
+    });
+  });
+
+  group("link image", () {
+    test("failed request", () async {
+      when(() => api.linkImage("url", meal)).thenAnswer((_) async => false);
+      expect(await images.linkImage("url", meal), "snackbar.linkImageError");
+    });
+
+    test("successful request", () async {
+      when(() => api.linkImage("url", meal)).thenAnswer((_) async => true);
+      expect(await images.linkImage("url", meal), "snackbar.linkImageSuccess");
+    });
+  });
+
+  group("report image", () {
+    test("failed report", () async {
+      when(() => api.reportImage(image, ReportCategory.noMeal))
+          .thenAnswer((_) async => false);
+      expect(await images.reportImage(image, ReportCategory.noMeal),
+          "snackbar.reportImageError");
+    });
+
+    test("successful report", () async {
+      when(() => api.reportImage(image, ReportCategory.noMeal))
+          .thenAnswer((_) async => true);
+      expect(await images.reportImage(image, ReportCategory.noMeal),
+          "snackbar.reportImageSuccess");
     });
   });
 }
