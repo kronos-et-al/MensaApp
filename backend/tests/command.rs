502b3df4189c1921a7b480552b7ee9ed4ed5a89e
Jonatan Ziegler <udslu@student.kit.edu>
2023-08-11 18:11:11 +0200

    test add image

--- backend/tests/command.rs
+++ backend/tests/command.rs
@@ -1,94 +1,56 @@
-use futures::Future;
 use mensa_app_backend::{
     interface::{
         api_command::{AuthInfo, Command, InnerAuthInfo},
-        persistent_data::{CommandDataAccess, MealplanManagementDataAccess},
+        persistent_data::CommandDataAccess,
     },
     layer::{
         data::{
-            database::{command::PersistentCommandData, factory::DataAccessFactory},
+            database::factory::{DataAccessFactory, DatabaseInfo},
             flickr_api::flickr_api_handler::FlickrApiHandler,
-            mail::{mail_info::MailInfo, mail_sender::MailSender},
+            mail::mail_sender::MailSender,
         },
         logic::api_command::command_handler::CommandHandler,
     },
     startup::config::ConfigReader,
-    util::{MealType, Uuid},
-};
-use sqlx::{
-    migrate::{MigrateDatabase, Migration},
-    Database, PgPool,
+    util::Uuid,
 };
 
 #[tokio::test]
 #[ignore = "manual test"]
-async fn test_command() {
-    use_database("name", |cmd, factory, pool| async move {
-        let insertion = factory.get_mealplan_management_data_access();
-        // let canteen = insertion.insert_canteen("canteen", 0).await.unwrap();
-        // let line = insertion.insert_line(canteen, "line", 0).await.unwrap();
-        let meal1 = insertion
-            .insert_meal("meal1", MealType::Beef, &[], &[])
-            .await
-            .unwrap();
+async fn test_add_image() {
+    let cmd = setup_cmd().await;
 
-        let client_id = Uuid::new_v4();
-        let auth_info = Some(InnerAuthInfo {
-            api_ident: API_KEY.into(),
-            client_id,
-            hash: "".into(),
-        });
+    let meal = Uuid::try_from("48b0ed7b-8387-46ad-866c-7993f469a9bf").unwrap();
 
-        cmd.add_image(meal1, "image_url".into(), auth_info)
-            .await
-            .unwrap();
-    })
-    .await;
-}
+    let auth_info = get_auth_info("T7X93M0t4oWRxRFxH2MpWdYSHZNsiqkkkpKbxL1AeD8wXR5pD+jmHvM4JjfD+WEx0Knl7g0DKSesmyzL2jVYxA==");
 
-const API_KEY: &str = "abcdefghij";
-
-async fn use_database<T: Future<Output = ()>>(
-    name: &str,
-    action: impl (FnOnce(
-        CommandHandler<PersistentCommandData, MailSender, FlickrApiHandler>,
-        DataAccessFactory,
-        PgPool,
-    ) -> T),
-) {
-    let reader = ConfigReader::default();
-    let mut info = reader.read_database_info().unwrap();
-    info.connection += "_command_test_";
-    info.connection += name;
-    let conn = info.connection.clone();
-
-    {
-        // setup database
-        if sqlx::Postgres::database_exists(&conn).await.unwrap() {
-            sqlx::Postgres::drop_database(&conn).await.unwrap();
-        }
-        sqlx::Postgres::create_database(&conn).await.unwrap();
-
-        let pool = PgPool::connect_lazy(&conn).unwrap();
-        sqlx::query!(
-            "INSERT INTO api_key(api_key, description) VALUES ($1, 'default')",
-            API_KEY
-        )
-        .execute(&pool)
+    cmd.add_image(meal, "https://flic.kr/p/2oSg8aV".into(), auth_info)
         .await
         .unwrap();
-        // setup cmd
+}
 
-        let mail = MailSender::new(reader.read_mail_info().unwrap()).unwrap();
-        let hoster = FlickrApiHandler::new(reader.read_flickr_info().unwrap());
+async fn setup_cmd() -> impl Command {
+    let reader = ConfigReader::default();
 
-        let factory = DataAccessFactory::new(info, true).await.unwrap();
-        let data = factory.get_command_data_access();
-        let cmd = CommandHandler::new(data, mail, hoster).await.unwrap();
 
-        action(cmd, factory, pool).await;
-    }
+    let mail = MailSender::new(reader.read_mail_info().unwrap()).unwrap();
+    let hoster = FlickrApiHandler::new(reader.read_flickr_info().unwrap());
 
-    // delete database out of scope
-    sqlx::Postgres::drop_database(&conn).await.unwrap();
+    let factory = DataAccessFactory::new(reader.read_database_info().unwrap(), true).await.unwrap();
+    let data = factory.get_command_data_access();
+    CommandHandler::new(data, mail, hoster).await.unwrap()
+}
+
+
+
+
+fn get_auth_info(hash: &str) -> AuthInfo {
+    let client_id = Uuid::try_from("6f04a6c7-9723-4a01-ae8c-67baa62fba75").unwrap();
+    const API_IDENT: &str = "1234567890";
+
+    Some(InnerAuthInfo {
+        api_ident: API_IDENT.into(),
+        client_id,
+        hash: hash.into(),
+    })
 }
