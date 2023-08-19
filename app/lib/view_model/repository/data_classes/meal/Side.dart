53104774c7805be44d317be90c93e2fdb9441c39
Elena Häußler <uqhnu@student.kit.edu>
2023-08-19 21:52:08 +0200

    delete toMap methods in repository

--- app/lib/view_model/repository/data_classes/meal/Side.dart
+++ app/lib/view_model/repository/data_classes/meal/Side.dart
@@ -63,28 +63,6 @@ class Side {
   /// @return The id of the side
   String get id => _id;
 
-  /// This method returns the additives as a map.
-  /// @return The additives as a map
-  List<Map<String, dynamic>> additiveToMap() {
-    return _additives
-        .map((additive) => {
-              'sideID': _id,
-              'additive': additive,
-            })
-        .toList();
-  }
-
-  /// This method returns the allerens as a map.
-  /// @return The allergens as a map
-  List<Map<String, dynamic>> allergenToMap() {
-    return _allergens
-        .map((allergen) => {
-              'sideID': _id,
-              'allergen': allergen,
-            })
-        .toList();
-  }
-
   /// This method returns the name of the side.
   /// @return The name of the side
   String get name => _name;
