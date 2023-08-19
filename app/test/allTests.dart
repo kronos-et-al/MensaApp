aa264a2f6b05708c414b90c98e0658afa6dca5de
Elena Häußler <uqhnu@student.kit.edu>
2023-08-19 18:57:09 +0200

    group test

--- app/test/allTests.dart
+++ app/test/allTests.dart
@@ -1,37 +1,41 @@
+import 'package:test/test.dart';
+
 import './view-model/repository/CanteenTest.dart' as canteen;
 import './view-model/repository/LineTest.dart' as line;
-import './view-model/repository/MealPlanTest.dart' as mealPlan;
+import './view-model/repository/MealPlanTest.dart' as meal_plan;
 import './view-model/repository/FilterPreferenceTest.dart' as filter;
 
 import './view-model/repository/MealTest.dart' as meal;
 import './view-model/repository/SideTest.dart' as side;
-import './view-model/repository/ImageDataTest.dart' as imageData;
+import './view-model/repository/ImageDataTest.dart' as image_data;
 import './view-model/repository/PriceTest.dart' as price;
 
 import './view-model/ImageAccessTest.dart' as image;
-import './view-model/MealPlanAccessTest.dart' as mealAccess;
+import './view-model/MealPlanAccessTest.dart' as meal_access;
 import './view-model/FavoriteMealAccessTest.dart' as favorite;
 import './view-model/PreferenceAccessTest.dart' as preference;
 
 import './model/SharedPreferencesTest.dart' as storage;
 import './model/api_server/GraphQlServerAccess_test.dart' as api;
+import './model/SqLiteAccessTest.dart' as database;
 
 void main() async {
-  canteen.main();
-  line.main();
-  mealPlan.main();
-  filter.main();
+  group("canteen", () => canteen.main());
+  group("line", () => line.main());
+  group("meal plan", () => meal_plan.main());
+  group("filter", () => filter.main());
 
-  imageData.main();
-  price.main();
-  side.main();
-  meal.main();
+  group("image", () => image_data.main());
+  group("price", () => price.main());
+  group("side", () => side.main());
+  group("meal", () => meal.main());
 
-  favorite.main();
-  mealAccess.main();
-  image.main();
-  preference.main();
+  group("favorite access", () => favorite.main());
+  group("meal plan access", () => meal_access.main());
+  group("image access", () => image.main());
+  group("preference access", () => preference.main());
 
-  await storage.main();
   api.main();
+  group("database", () => database.main());
+  await storage.main();
 }
