05ca496cc71455255fd3a30dd6d30b9417beaebe
Jonatan Ziegler <udslu@student.kit.edu>
2023-08-20 09:55:56 +0200

    int command

--- backend/tests/command.rs
+++ backend/tests/command.rs
@@ -1,18 +1,14 @@
 use mensa_app_backend::{
-    interface::{
-        api_command::{AuthInfo, Command, InnerAuthInfo},
-        persistent_data::CommandDataAccess,
-    },
+    interface::api_command::{AuthInfo, Command, InnerAuthInfo},
     layer::{
         data::{
-            database::factory::{DataAccessFactory, DatabaseInfo},
-            flickr_api::flickr_api_handler::FlickrApiHandler,
+            database::factory::DataAccessFactory, flickr_api::flickr_api_handler::FlickrApiHandler,
             mail::mail_sender::MailSender,
         },
         logic::api_command::command_handler::CommandHandler,
     },
     startup::config::ConfigReader,
-    util::{Uuid, ReportReason},
+    util::{ReportReason, Uuid},
 };
 
 #[tokio::test]
@@ -22,13 +18,14 @@ async fn test_add_image() {
 
     let meal = Uuid::try_from("48b0ed7b-8387-46ad-866c-7993f469a9bf").unwrap();
 
-    let auth_info = get_auth_info("T7X93M0t4oWRxRFxH2MpWdYSHZNsiqkkkpKbxL1AeD8wXR5pD+jmHvM4JjfD+WEx0Knl7g0DKSesmyzL2jVYxA==");
+    let auth_info = get_auth_info(
+        "T7X93M0t4oWRxRFxH2MpWdYSHZNsiqkkkpKbxL1AeD8wXR5pD+jmHvM4JjfD+WEx0Knl7g0DKSesmyzL2jVYxA==",
+    );
 
     cmd.add_image(meal, "https://flic.kr/p/2oSg8aV".into(), auth_info)
         .await
         .unwrap();
 }
-
 
 #[tokio::test]
 #[ignore = "manual test"]
@@ -39,25 +36,101 @@ async fn test_report_image() {
 
     let reason = ReportReason::NoMeal;
 
-    let auth_info = get_auth_info("rxGJ5jz4aMwklyzFxZq6eHlySLuuPyv2+ixnzz8hDB2UQ+KuiJOaLKtINJh/iPZmgAEipA7E/ADceiFSO1RWmQ==");
+    let auth_info = get_auth_info(
+        "rxGJ5jz4aMwklyzFxZq6eHlySLuuPyv2+ixnzz8hDB2UQ+KuiJOaLKtINJh/iPZmgAEipA7E/ADceiFSO1RWmQ==",
+    );
 
     cmd.report_image(image_id, reason, auth_info).await.unwrap();
 }
 
+#[tokio::test]
+#[ignore = "manual test"]
+async fn test_set_meal_rating() {
+    let cmd = setup_cmd().await;
 
+    let meal_id = Uuid::try_from("48b0ed7b-8387-46ad-866c-7993f469a9bf").unwrap();
+
+    let rating = 4;
+
+    let auth_info = get_auth_info(
+        "YXBIcDFORrhY83pBmwuSwb0l0zsYXM/h18OfT+AMEYCXWCtpExlN2EMiLtjGkv7w2/cAPmIKe8m+mQbGasbcEQ==",
+    );
+
+    cmd.set_meal_rating(meal_id, rating, auth_info)
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+#[ignore = "manual test"]
+async fn test_add_image_upvote() {
+    let cmd = setup_cmd().await;
+
+    let image_id = Uuid::try_from("1b8f373b-7383-4a3a-9818-e0137fd164b7").unwrap();
+
+    let auth_info = get_auth_info(
+        "r+tenhKxOfa2wJUE+EPzOyRgtIc5IBDjh/iMyvlP4vznoiLLsJKOMyKOs60ZV98oPrOEMpyDnONdavwFgnoH+g==",
+    );
+
+    cmd.add_image_upvote(image_id, auth_info).await.unwrap();
+}
+
+#[tokio::test]
+#[ignore = "manual test"]
+async fn test_add_image_dovnvote() {
+    let cmd = setup_cmd().await;
+
+    let image_id = Uuid::try_from("1b8f373b-7383-4a3a-9818-e0137fd164b7").unwrap();
+
+    let auth_info = get_auth_info(
+        "aKlyzgxz/0pHK+LWQFHhWoyhUjOKa+/fmr6my4VH3Rxn+lPvvXb4lk3rENx8TSwCd2Sjw7qspPrs2S93ZoG/fQ==",
+    );
+
+    cmd.add_image_downvote(image_id, auth_info).await.unwrap();
+}
+
+#[tokio::test]
+#[ignore = "manual test"]
+async fn test_remove_image_upvote() {
+    let cmd = setup_cmd().await;
+
+    let image_id = Uuid::try_from("1b8f373b-7383-4a3a-9818-e0137fd164b7").unwrap();
+
+    let auth_info = get_auth_info(
+        "aNN/2nk2JgE0wZOMWG0zzxg/LtqaCeiLhjq7ebqDrnSSXtWVJC9kItKk1/RBKnzD+T+gGH/JGclBy8SWdgBKMQ==",
+    );
+
+    cmd.remove_image_upvote(image_id, auth_info).await.unwrap();
+}
+
+#[tokio::test]
+#[ignore = "manual test"]
+async fn test_remove_image_downvote() {
+    let cmd = setup_cmd().await;
+
+    let image_id = Uuid::try_from("1b8f373b-7383-4a3a-9818-e0137fd164b7").unwrap();
+
+    let auth_info = get_auth_info(
+        "65KlFbT+MijU8OqUWOleQZ/oxikIGp7q4hbEYTlEMnjbylrzNnl8nZUa1LEFaKZptAW46PmGJ/Xgcm6p4FzQxg==",
+    );
+
+    cmd.remove_image_downvote(image_id, auth_info)
+        .await
+        .unwrap();
+}
 
 async fn setup_cmd() -> impl Command {
     let reader = ConfigReader::default();
 
-
     let mail = MailSender::new(reader.read_mail_info().unwrap()).unwrap();
     let hoster = FlickrApiHandler::new(reader.read_flickr_info().unwrap());
 
-    let factory = DataAccessFactory::new(reader.read_database_info().unwrap(), true).await.unwrap();
+    let factory = DataAccessFactory::new(reader.read_database_info().unwrap(), true)
+        .await
+        .unwrap();
     let data = factory.get_command_data_access();
     CommandHandler::new(data, mail, hoster).await.unwrap()
 }
-
 
 fn get_auth_info(hash: &str) -> AuthInfo {
     let client_id = Uuid::try_from("6f04a6c7-9723-4a01-ae8c-67baa62fba75").unwrap();
