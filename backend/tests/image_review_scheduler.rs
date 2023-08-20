e298ef3ec0297c00d58a8aac4b72dfc29274cab4
uglrl <134957879+uglrl@users.noreply.github.com>
2023-08-20 15:04:17 +0200

    Added mock for the parser

--- backend/tests/image_review_scheduler.rs
+++ backend/tests/image_review_scheduler.rs
@@ -1,39 +1,51 @@
+use async_trait::async_trait;
 use mensa_app_backend::{
+    interface::mealplan_management::MensaParseScheduling,
     layer::{
         data::{
             database::factory::DataAccessFactory, flickr_api::flickr_api_handler::FlickrApiHandler,
-            swka_parser::swka_parse_manager::SwKaParseManager,
         },
-        logic::{
-            image_review::image_reviewer::ImageReviewer,
-            mealplan_management::meal_plan_manager::MealPlanManager,
-        },
+        logic::image_review::image_reviewer::ImageReviewer,
         trigger::scheduling::scheduler::{ScheduleInfo, Scheduler},
     },
     startup::config::ConfigReader,
 };
+use tokio::signal::ctrl_c;
 
 #[tokio::test]
-async fn test_image_scheduling() {}
+async fn test_image_scheduling() {
+    let mut scheduler = setup().await;
+    scheduler.start().await;
+    ctrl_c().await.unwrap();
+    scheduler.shutdown().await;
+}
 
 async fn setup() -> Scheduler {
     let info = ScheduleInfo {
-        full_parse_schedule: "*/1 * * * * *".to_string(),
-        update_parse_schedule: "*/2 * * * * *".to_string(),
+        full_parse_schedule: "* * * * * 0".to_string(),
+        update_parse_schedule: "* * * * * 0".to_string(),
         image_review_schedule: "*/5 * * * * *".to_string(),
     };
     let reader = ConfigReader::default();
-
-    let image_hoster = FlickrApiHandler::new(reader.read_flickr_info().unwrap());
-    let meal_plan_parser = SwKaParseManager::new(reader.read_swka_info().unwrap()).unwrap();
     let database_factory = DataAccessFactory::new(reader.read_database_info().unwrap(), false)
         .await
         .unwrap();
     let image_review_data = database_factory.get_image_review_data_access();
-    let mealplan_management_data = database_factory.get_mealplan_management_data_access();
-
+    let image_hoster = FlickrApiHandler::new(reader.read_flickr_info().unwrap());
     let image_scheduling = ImageReviewer::new(image_review_data, image_hoster);
+    Scheduler::new(info, image_scheduling, MealplanManagerMock::default()).await
+}
 
-    let parse_scheduling = MealPlanManager::new(mealplan_management_data, meal_plan_parser);
-    Scheduler::new(info, image_scheduling, parse_scheduling).await
+#[derive(Default)]
+struct MealplanManagerMock {}
+
+#[async_trait]
+impl MensaParseScheduling for MealplanManagerMock {
+    async fn start_update_parsing(&self) {
+        return;
+    }
+
+    async fn start_full_parsing(&self) {
+        return;
+    }
 }
