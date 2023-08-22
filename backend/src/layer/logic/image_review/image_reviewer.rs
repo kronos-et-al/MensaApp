1b439af8a2432dbeba4c3060aaae91863597ca9a
uglrl <134957879+uglrl@users.noreply.github.com>
2023-08-22 17:38:27 +0200

    Changed warn to error

--- backend/src/layer/logic/image_review/image_reviewer.rs
+++ backend/src/layer/logic/image_review/image_reviewer.rs
@@ -2,7 +2,7 @@ use async_trait::async_trait;
 use chrono::Local;
 use futures::future::join_all;
 use thiserror::Error;
-use tracing::warn;
+use tracing::{warn, error};
 
 use crate::interface::{
     image_hoster::{ImageHoster, ImageHosterError},
@@ -40,7 +40,7 @@ where
 {
     async fn start_image_review(&self) {
         if let Err(error) = self.try_start_image_review().await {
-            warn!("Error reviewing images: {error}");
+            error!("Error reviewing images: {error}");
         }
     }
 }
