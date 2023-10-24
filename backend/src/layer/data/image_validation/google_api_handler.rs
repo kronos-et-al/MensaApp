e14de5fca5fbf287621e0d4a4d412d805e6d6c49
Whatsuup <alexanderalbers01@gmail.com>
2023-10-24 18:53:50 +0200

    implemented google_api_handler.rs

--- backend/src/layer/data/image_validation/google_api_handler.rs
+++ backend/src/layer/data/image_validation/google_api_handler.rs
@@ -1,29 +1,40 @@
-use crate::interface::image_validation::{ImageValidation, ImageValidationInfo};
+use std::io::Cursor;
 use crate::interface::image_validation::Result;
-use crate::util::ImageResource;
-use async_trait::async_trait;
+use crate::interface::image_validation::{ImageValidation, ImageValidationInfo};
 use crate::layer::data::image_validation::api_request::ApiRequest;
 use crate::layer::data::image_validation::image_evaluation::ImageEvaluation;
+use crate::util::ImageResource;
+use async_trait::async_trait;
+use image::ImageOutputFormat;
 
-//todo
+//TODO DOC
 pub struct GoogleApiHandler {
     evaluation: ImageEvaluation,
     request: ApiRequest,
 }
 
 impl GoogleApiHandler {
-    #[must_use]
-    pub fn new(info: ImageValidationInfo) -> Self {
-        Self {
+    //TODO DOC
+    pub fn new(info: ImageValidationInfo) -> Result<Self> {
+        Ok(Self {
             evaluation: ImageEvaluation::new(info.acceptance),
-            request: ApiRequest::new(info.api_key, info.project_key),
-        }
+            request: ApiRequest::new(info.json_path, info.project_id)?,
+        })
     }
 }
 
 #[async_trait]
 impl ImageValidation for GoogleApiHandler {
     async fn validate_image(&self, image: &ImageResource) -> Result<()> {
-        todo!()
+        let b64_image = image_to_base64(image);
+        let results = self.request.encoded_image_validation(b64_image).await?;
+        self.evaluation.verify(results)
     }
+}
+
+fn image_to_base64(img: &ImageResource) -> String {
+    let mut image_data: Vec<u8> = Vec::new();
+    img.write_to(&mut Cursor::new(&mut image_data), ImageOutputFormat::Png)
+        .unwrap();
+    base64::encode(image_data) // TODO depreciated
 }
