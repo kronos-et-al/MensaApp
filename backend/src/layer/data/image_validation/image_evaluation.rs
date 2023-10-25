0eaaf6957ea076de7d3fa38a298f05b741d0af34
Whatsuup <alexanderalbers01@gmail.com>
2023-10-25 09:32:06 +0200

    added image_evaluation docs

--- backend/src/layer/data/image_validation/image_evaluation.rs
+++ backend/src/layer/data/image_validation/image_evaluation.rs
@@ -2,19 +2,43 @@ use crate::interface::image_validation::ImageValidationError::InvalidContent;
 use crate::interface::image_validation::Result;
 use crate::layer::data::image_validation::json_structs::SafeSearchJson;
 
-//TODO DOC
+/// The [`ImageEvaluation`] struct is used to verify and
+/// determine SafeSearch values from the [`ApiRequest`] class.
 pub struct ImageEvaluation {
     acceptance: [u8; 5],
 }
 
 impl ImageEvaluation {
-    //TODO DOC
+    /// This method creates a new instance of the [`ImageEvaluation`] struct
+    /// # Params
+    /// #### acceptance: [u8; 5]
+    /// The acceptance array contains all allowed value levels
+    /// of the five **safe-search categories** in the following order:<br>
+    /// **adult**, **spoof**, **medical**, **violence** and **racy**.<br>
+    /// There are five different **levels** for each category:
+    /// **VERY_UNLIKELY**=1, **UNLIKELY**=2, **POSSIBLE**=3, **LIKELY**=4 and **VERY_LIKELY**=5.
+    /// #### Example
+    /// If we want to block only adult content, the following acceptance array is the right joice:<br>
+    /// **[1,5,5,5,5]**<br>
+    /// Now the first category (adult) has a maximum level of 1 which means, every input that is
+    /// greater that 1 in this category will not be accepted. All other categories are at the maximum level,
+    /// which means every level is accepted.
     #[must_use]
     pub const fn new(acceptance: [u8; 5]) -> Self {
         Self { acceptance }
     }
 
-    //TODO DOC
+    /// This method checks if the input values are allowed with the max level configuration for each category.
+    /// that is set in the relation [`ImageEvaluation`] struct.
+    /// For more information about levels and categories see [`ImageEvaluation::new`].
+    /// # Params
+    /// #### results: SafeSearchJson
+    /// This param contains all the evaluated levels for each category.
+    /// # Error
+    /// This method returns an error if the proved results do not match with the configuration.
+    /// The error message contains the failing category.
+    /// # Return
+    /// Nothing if the values match with the configuration.
     pub fn verify(&self, results: SafeSearchJson) -> Result<()> {
         let values = result_to_arr(&results);
         for (i, value) in values.iter().enumerate() {
