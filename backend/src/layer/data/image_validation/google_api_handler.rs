49d2a72a3f59b7e3e9188d3303b0d0721c052f4f
Whatsuup <alexanderalbers01@gmail.com>
2023-10-25 10:22:58 +0200

    added google_api_handler docs

--- backend/src/layer/data/image_validation/google_api_handler.rs
+++ backend/src/layer/data/image_validation/google_api_handler.rs
@@ -8,14 +8,30 @@ use async_trait::async_trait;
 use image::ImageOutputFormat;
 use std::io::Cursor;
 
-//TODO DOC
+/// The [`GoogleApiHandler`] struct is used to manage tasks
+/// of the [`crate::layer::data::image_validation`] component.
+/// These tasks are:<br>
+///     - converting images<br>
+///     - calling the api<br>
+///     - determine which images are allowed<br>
+///     - and returning helpful error messages if an image got not accepted
 pub struct GoogleApiHandler {
     evaluation: ImageEvaluation,
     request: ApiRequest,
 }
 
 impl GoogleApiHandler {
-    //TODO DOC
+    /// This method creates a new instance of the [`GoogleApiHandler`].
+    /// # Params
+    /// `info`<br>
+    /// This struct contains all information :) that is needed to setup this struct like
+    /// authentication information and acceptance level for the image evaluation.
+    /// # Errors
+    /// It is possible that the [`ApiRequest`] creation fails, as the provided information causes failures.
+    /// These failures will be returned as an error.
+    /// See [`crate::interface::image_validation::ImageValidationError`] for more info about the errors.
+    /// # Return
+    /// The mentioned [`GoogleApiHandler`] struct.
     pub fn new(info: ImageValidationInfo) -> Result<Self> {
         Ok(Self {
             evaluation: ImageEvaluation::new(info.acceptance),
@@ -29,7 +45,7 @@ impl ImageValidation for GoogleApiHandler {
     async fn validate_image(&self, image: &ImageResource) -> Result<()> {
         let b64_image = image_to_base64(image)?;
         let results = self.request.encoded_image_validation(b64_image).await?;
-        self.evaluation.verify(results)
+        self.evaluation.verify(&results)
     }
 }
 
