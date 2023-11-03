416e4fc735d0e604646bd58087bd5b207555b08f
Whatsuup <alexanderalbers01@gmail.com>
2023-11-03 10:19:15 +0100

    test change

--- backend/src/layer/data/database/mealplan_management.rs
+++ backend/src/layer/data/database/mealplan_management.rs
@@ -499,7 +499,7 @@ mod test {
             ("f7337122-b018-48ad-b420-6202dc3cb4ff", (vec![], vec![We])),
             (
                 "25cb8c50-75a4-48a2-b4cf-8ab2566d8bec",
-                (vec![], vec![Ei, ML, We]),
+                (vec![], vec![Ei, ML, So]),
             ),
             (
                 "0a850476-eda4-4fd8-9f93-579eb85b8c25",
