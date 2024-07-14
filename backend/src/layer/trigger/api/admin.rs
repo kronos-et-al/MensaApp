1dca4708f25d4eacf0b1db5cfaf09bc29dcf6e33
Jonatan Ziegler <udslu@student.kit.edu>
2024-07-14 12:29:31 +0200

    admin api test

--- backend/src/layer/trigger/api/admin.rs
+++ backend/src/layer/trigger/api/admin.rs
@@ -106,3 +106,115 @@ pub(super) async fn admin_auth_middleware(
 
     Ok(next.run(req).await)
 }
+
+#[cfg(test)]
+#[allow(clippy::unwrap_used)]
+mod test {
+    use std::{
+        net::{Ipv4Addr, SocketAddr, SocketAddrV4},
+        sync::Arc,
+    };
+
+    use axum::{http::HeaderValue, Server};
+    use base64::Engine;
+    use hyper::{header::AUTHORIZATION, HeaderMap, StatusCode};
+    use reqwest::Client;
+
+    use super::ADMIN_USER;
+    use crate::{
+        layer::trigger::api::{admin::admin_router, mock::CommandMock},
+        util::Uuid,
+    };
+
+    #[tokio::test]
+    async fn test_api() {
+        let key: String = "asdasdasdasd".into();
+        let command = Arc::new(CommandMock);
+
+        let router = admin_router(key.clone(), command);
+        let socket = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 8081));
+        println!("socket: {socket}");
+        let server = Server::bind(&socket).serve(router.into_make_service());
+
+        tokio::spawn(server);
+
+        assert_eq!(
+            StatusCode::UNAUTHORIZED,
+            reqwest::get(format!("http://{socket}/version"))
+                .await
+                .unwrap()
+                .status()
+        );
+
+        let auth_header = build_auth_string(ADMIN_USER, &key);
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, auth_header);
+        let authed_client = Client::builder().default_headers(headers).build().unwrap();
+
+        let version = authed_client
+            .get(format!("http://{socket}/version"))
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+
+        assert_eq!(env!("CARGO_PKG_VERSION"), version);
+
+        let id = Uuid::default();
+
+        assert_eq!(
+            StatusCode::OK,
+            authed_client
+                .get(format!("http://{socket}/report/delete_image/{id}"))
+                .send()
+                .await
+                .unwrap()
+                .status()
+        );
+
+        assert_eq!(
+            StatusCode::OK,
+            authed_client
+                .get(format!("http://{socket}/report/verify_image/{id}"))
+                .send()
+                .await
+                .unwrap()
+                .status()
+        );
+
+        assert_eq!(
+            StatusCode::UNAUTHORIZED,
+            authed_client
+                .get(format!("http://{socket}/version"))
+                .header(
+                    AUTHORIZATION.to_string(),
+                    build_auth_string(ADMIN_USER, "invalid")
+                )
+                .send()
+                .await
+                .unwrap()
+                .status()
+        );
+        assert_eq!(
+            StatusCode::UNAUTHORIZED,
+            authed_client
+                .get(format!("http://{socket}/version"))
+                .header(
+                    AUTHORIZATION.to_string(),
+                    build_auth_string("wrong_user", &key)
+                )
+                .send()
+                .await
+                .unwrap()
+                .status()
+        );
+    }
+
+    fn build_auth_string(username: &str, password: &str) -> HeaderValue {
+        let auth_string = format!("{username}:{password}");
+        let auth_string = base64::engine::general_purpose::STANDARD.encode(auth_string);
+        HeaderValue::from_str(&format!("Basic {auth_string}")).unwrap()
+    }
+}
