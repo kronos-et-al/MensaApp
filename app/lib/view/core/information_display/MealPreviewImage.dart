a2ca7644c747def3a3bfde4ebac1b76bf51a7bfd
Jonatan Ziegler <udslu@student.kit.edu>
2025-03-03 20:34:21 +0100

    feat: frontend: image dots
    closes #165

--- app/lib/view/core/information_display/MealPreviewImage.dart
+++ app/lib/view/core/information_display/MealPreviewImage.dart
@@ -7,6 +7,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 
+const _maxImageDots = 5;
+
 /// Displays a Meal's image.
 class MealPreviewImage extends StatelessWidget {
   final Meal _meal;
@@ -20,7 +22,8 @@ class MealPreviewImage extends StatelessWidget {
   final Alignment _favoriteButtonAlignment;
 
   /// Creates a MealPreviewImage.
-  const MealPreviewImage({super.key,
+  const MealPreviewImage({
+    super.key,
     required Meal meal,
     bool displayFavorite = false,
     bool enableUploadButton = false,
@@ -30,16 +33,16 @@ class MealPreviewImage extends StatelessWidget {
     double? height,
     double? width,
     Function? onUploadButtonPressed,
-    void Function()? onImagePressed})
-      : _meal = meal,
-        _enableUploadButton = enableUploadButton,
-        _enableFavoriteButton = enableFavoriteButton,
-        _borderRadius = borderRadius ?? const BorderRadius.all(Radius.zero),
-        _height = height,
-        _width = width,
-        _onUploadButtonPressed = onUploadButtonPressed,
-        _onImagePressed = onImagePressed,
-        _favoriteButtonAlignment =favoriteButtonAlignment;
+    void Function()? onImagePressed,
+  }) : _meal = meal,
+       _enableUploadButton = enableUploadButton,
+       _enableFavoriteButton = enableFavoriteButton,
+       _borderRadius = borderRadius ?? const BorderRadius.all(Radius.zero),
+       _height = height,
+       _width = width,
+       _onUploadButtonPressed = onUploadButtonPressed,
+       _onImagePressed = onImagePressed,
+       _favoriteButtonAlignment = favoriteButtonAlignment;
 
   @override
   Widget build(BuildContext context) {
@@ -48,128 +51,201 @@ class MealPreviewImage extends StatelessWidget {
         _meal.images!.isEmpty ||
         _meal.images!.first.url.isEmpty) {
       return Container(
-          width: _width,
-          height: _height,
-          decoration: BoxDecoration(
-              borderRadius: _borderRadius, color: theme.colorScheme.primary),
-          child: ClipRRect(
-              borderRadius: _borderRadius,
-              child: Stack(children: [
-                Center(
-                    child: Column(
+        width: _width,
+        height: _height,
+        decoration: BoxDecoration(
+          borderRadius: _borderRadius,
+          color: theme.colorScheme.primary,
+        ),
+        child: ClipRRect(
+          borderRadius: _borderRadius,
+          child: Stack(
+            children: [
+              Center(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     LogoIcon(size: min(96, _height! - 16)),
                     if (_enableUploadButton) const SizedBox(height: 16),
                     if (_enableUploadButton)
                       MensaButton(
-                          semanticLabel: FlutterI18n.translate(
-                              context, "semantics.imageUpload"),
-                          onPressed: _onUploadButtonPressed,
-                          text: "Bild hochladen")
-                  ],
-                )),
-                if (_enableFavoriteButton && _meal.isFavorite)
-                  Align(
-                      alignment: _favoriteButtonAlignment,
-                      child: Padding(
-                        padding: const EdgeInsets.all(4),
-                        child: Stack(
-                          children: [
-                            Icon(Icons.favorite,
-                                size: 28,
-                                color: Theme
-                                    .of(context)
-                                    .colorScheme
-                                    .onSurface),
-                            Icon(Icons.favorite_border,
-                                size: 28,
-                                color:
-                                    Theme.of(context).colorScheme.surface),
-                          ],
+                        semanticLabel: FlutterI18n.translate(
+                          context,
+                          "semantics.imageUpload",
                         ),
-                      )),
-              ])));
+                        onPressed: _onUploadButtonPressed,
+                        text: "Bild hochladen",
+                      ),
+                  ],
+                ),
+              ),
+              if (_enableFavoriteButton && _meal.isFavorite)
+                Align(
+                  alignment: _favoriteButtonAlignment,
+                  child: Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: Stack(
+                      children: [
+                        Icon(
+                          Icons.favorite,
+                          size: 28,
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                        Icon(
+                          Icons.favorite_border,
+                          size: 28,
+                          color: Theme.of(context).colorScheme.surface,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      );
     } else {
       return Material(
+        borderRadius: _borderRadius,
+        child: InkWell(
           borderRadius: _borderRadius,
-          child: InkWell(
-              borderRadius: _borderRadius,
-              onTap: _onImagePressed,
-              child: CachedNetworkImage(
-                imageUrl: _meal.images!.first.url,
-                placeholder: (context, url) => Container(
-                    width: _width,
-                    height: _height,
-                    decoration: BoxDecoration(
-                        borderRadius: _borderRadius,
-                        color: theme.colorScheme.primary),
-                    child: ClipRRect(
-                        borderRadius: _borderRadius,
-                        child: Stack(children: [
-                          Center(
-                              child: Column(
+          onTap: _onImagePressed,
+          child: CachedNetworkImage(
+            imageUrl: _meal.images!.first.url,
+            placeholder:
+                (context, url) => Container(
+                  width: _width,
+                  height: _height,
+                  decoration: BoxDecoration(
+                    borderRadius: _borderRadius,
+                    color: theme.colorScheme.primary,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: _borderRadius,
+                    child: Stack(
+                      children: [
+                        Center(
+                          child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              LogoIcon(size: min(96, _height! - 16)),
-                            ],
-                          )),
-                          if (_enableFavoriteButton && _meal.isFavorite)
-                            Align(
-                                alignment: _favoriteButtonAlignment,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4),
-                                  child: Stack(
-                                    children: [
-                                      Icon(Icons.favorite,
-                                          size: 28,
-                                          color: Theme
-                                              .of(context)
-                                              .colorScheme
-                                              .onSurface),
-                                      Icon(Icons.favorite_border,
-                                          size: 28,
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .surfaceDim),
-                                    ],
-                                  ),
-                                ))
-                        ]))),
-                imageBuilder: (context, imageProvider) => Container(
-                    width: _width,
-                    height: _height,
-                    decoration: BoxDecoration(
-                      borderRadius: _borderRadius,
-                      image: DecorationImage(
-                        image: imageProvider,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Stack(children: [
-                      if (_enableFavoriteButton && _meal.isFavorite)
-                        Align(
+                            children: [LogoIcon(size: min(96, _height! - 16))],
+                          ),
+                        ),
+                        if (_enableFavoriteButton && _meal.isFavorite)
+                          Align(
                             alignment: _favoriteButtonAlignment,
                             child: Padding(
                               padding: const EdgeInsets.all(4),
                               child: Stack(
                                 children: [
-                                  Icon(Icons.favorite,
-                                      size: 28,
-                                      color: Theme
-                                          .of(context)
-                                          .colorScheme
-                                          .primary),
-                                  Icon(Icons.favorite_border,
-                                      size: 28,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .surfaceDim),
+                                  Icon(
+                                    Icons.favorite,
+                                    size: 28,
+                                    color:
+                                        Theme.of(context).colorScheme.onSurface,
+                                  ),
+                                  Icon(
+                                    Icons.favorite_border,
+                                    size: 28,
+                                    color:
+                                        Theme.of(
+                                          context,
+                                        ).colorScheme.surfaceDim,
+                                  ),
                                 ],
                               ),
-                            )),
-                    ])),
-              )));
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+            imageBuilder:
+                (context, imageProvider) => Container(
+                  width: _width,
+                  height: _height,
+                  decoration: BoxDecoration(
+                    borderRadius: _borderRadius,
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      if (_enableFavoriteButton && _meal.isFavorite)
+                        Align(
+                          alignment: _favoriteButtonAlignment,
+                          child: Padding(
+                            padding: const EdgeInsets.all(4),
+                            child: Stack(
+                              children: [
+                                Icon(
+                                  Icons.favorite,
+                                  size: 28,
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
+                                Icon(
+                                  Icons.favorite_border,
+                                  size: 28,
+                                  color:
+                                      Theme.of(context).colorScheme.surfaceDim,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      if ((_meal.images?.length ?? 0) > 1 &&
+                          (_meal.images?.length ?? 0) <= _maxImageDots)
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: List<Widget>.generate(
+                              _meal.images!.length,
+                              (_) => Container(
+                                height: 5,
+                                width: 5,
+                                margin:
+                                    EdgeInsets.all(1.5) +
+                                    EdgeInsets.only(bottom: 4),
+                                decoration: BoxDecoration(
+                                  color: Color(0xFFF7F7F7),
+                                  shape: BoxShape.circle,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black45,
+                                      blurRadius: 2,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      if ((_meal.images?.length ?? 0) > _maxImageDots)
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            height: 4,
+                            width: 8 * _maxImageDots.toDouble(),
+                            margin: EdgeInsets.only(bottom: 5.5),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(2.5),
+                              ),
+                              color: Color(0xFFF7F7F7),
+                              boxShadow: [
+                                BoxShadow(color: Colors.black45, blurRadius: 2),
+                              ],
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+          ),
+        ),
+      );
     }
   }
 }
